dfa2e3a4993fac1a61d7bf870791b7d24a515f2e
camondino <84874180+camondino@users.noreply.github.com>
2023-05-04 20:50:35 -0300

    Arbol funcinando con grafico y matriz de confusion con mapa de color
    Falta agregar validacion cruzada y empezar con random forest

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 # from dspy.data_preparation import clean_data
 # from dspy.modeling import naive_bayes, test_design
@@ -9,39 +10,63 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
 
 # Levanto dataset
 df = pd.read_excel('data_preparation/df_prepared.xlsx', index_col=0)
+df = df.dropna()  # Elimina filas con al menos un valor nulo
 print(df.head())
 
-# Divido en train y test
-X = df.drop('equipo_ganador', axis=1)
-y = df['equipo_ganador']
+N_MODELOS = 15
+l_aciertos = []
 
-# Codificamos las variables categoricas string en numericas
-labelencoder = LabelEncoder()
-for column in X.columns:
-    X[column] = labelencoder.fit_transform(X[column])
+for i in range(N_MODELOS):
+    # Shuffle dataset
+    df = df.sample(frac=1).reset_index(drop=True)
 
-# Dividimos datos
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Divido en train y test
+    X = df.drop('equipo_ganador', axis=1)
+    y = df['equipo_ganador']
 
-# Entrenamiento
-modelo = DecisionTreeClassifier(max_depth=3)
-modelo.fit(X_train, y_train)
+    # Codificamos las variables categoricas string en numericas
+    labelencoder = LabelEncoder()
+    cat_columns = ['equipo_loc', 'equipo_vis', 'arbitro', 'dt_loc', 'dt_vis']
+    for column in cat_columns:
+        X[column] = labelencoder.fit_transform(X[column])
+    
+    print(X)
+    print(y)
 
-# Predicciones
-y_pred = modelo.predict(X_test)
-precision = accuracy_score(y_test, y_pred)
-matriz_confusion = confusion_matrix(y_test, y_pred)
+    # Dividimos datos
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print("precision: ", precision)
-print(matriz_confusion)
+    # Entrenamiento
+    modelo = DecisionTreeClassifier(max_depth=6)
+    modelo.fit(X_train, y_train)
 
-# Graficar el árbol
-fig, ax = plt.subplots(figsize=(10, 6))
-plot_tree(modelo, feature_names=X.columns, class_names=y.unique(), filled=True, ax=ax)
-plt.show()
+    # Predicciones
+    y_pred = modelo.predict(X_test)
+    precision = accuracy_score(y_test, y_pred)
+    l_aciertos.append(precision)
+    matriz_confusion = confusion_matrix(y_test, y_pred, labels=np.unique(y_pred))
+    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=matriz_confusion,
+                                                display_labels=["Local", "Empate", "Visitante"])
+    cm_display.plot(cmap='Blues')
+
+    print("precision: ", precision)
+    print(matriz_confusion)
+
+    # Graficar el árbol
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plot_tree(modelo, feature_names=X.columns, class_names=y.unique(), filled=True, ax=ax)
+    plt.show()
+    
+
+    
+print(f"Max: {max(l_aciertos)} Min: {min(l_aciertos)} Prom: {sum(l_aciertos)/len(l_aciertos)}")
+
 
 # Vamos a usar modelos de librerias independientes de la libreria que hizo Nacho
 def main():
@@ -75,4 +100,4 @@ def main():
 
     # print(f"Max: {max(l_aciertos)} Min: {min(l_aciertos)} Prom: {sum(l_aciertos)/len(l_aciertos)}")
 
-main()
+# main()
