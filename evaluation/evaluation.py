e2ab549df6b0b4df5c6e25ac7a6f2449de6c191f
nachomondino1 <nachomondino1@gmail.com>
2023-05-04 11:54:25 -0300

    Quite los modelos que fallaban como la red neuronal y xgboost. Pase el naive propio a la libreria dsmlpy y pase lo mismo de def main() a main.py
    En evaluation.py puse el calculo de la precision del modelo de Naive

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -2,6 +2,19 @@ import pandas as pd
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
+def calculate_precision(df_result, var_resp):
+
+    n_aciertos, prob_certeza = 0, 0
+    for i in range(len(df_result)):
+
+        # Si el modelo predijo bien
+        if df_result.loc[i, var_resp] == df_result.loc[i, 'y_pred']:
+            n_aciertos += 1
+            prob_certeza += df_result.loc[i, 'y_pred_prob']
+
+    # Guardo resultados del modelo
+    return n_aciertos / len(df_result) * 100
+
 # MATRIZ DE CONFUSION
 def confusion_matrix(df):
     confusion_matrix = metrics.confusion_matrix(df['equipo_ganador'], df['y_pred'])
