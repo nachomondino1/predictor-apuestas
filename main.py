49f3faf6af75ad94a6e0ad2f459ae17315f112d8
nachomondino1 <nachomondino1@gmail.com>
2023-05-04 12:08:51 -0300

    Quite todo lo que correspondia a un archivo a parte como el modeling Separe mejor entre etapas

--- main.py
+++ main.py
@@ -1,103 +1,87 @@
 # Importo librerias
 import pandas as pd
-from sklearn import metrics
-import matplotlib.pyplot as plt
-from data_preparation.clean_data import clean_teams
-from data_preparation import integrate_data
-from dspy.data_understanding import describe_data
-from dspy.data_preparation import format_data, clean_data
+from dspy.data_preparation import clean_data
+from data_preparation import construct_data, format_data, select_data
 from dspy.modeling import naive_bayes, test_design
+from evaluation.evaluation import calculate_precision
 
-
-def main():
-    # La idea es poner toda el camino de los datos aqui...
-    # Levanto el dataset
-    df = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/flashscore/liga_argentina_historico.xlsx')
+def main():  # La idea es poner toda el camino de los datos aqui...
+    # Hiperparametros
+    N_ULT_PART = 5
 
     # 2) DATA UNDERSTANDING
-    # 2.1) Describe data
-    describe_data.getting_to_know_data(df)
-
-    # 2.2) Format data
-    df = format_data.format_column_date(df)
+    # Levanto el dataset
+    df = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/liga_argentina_historico.xlsx')
+    # Describe y explore data en Google Colab con la libreria ydata-profiling
 
     # 3) DATA PREPARATION
-    # 3.1) Clean data
-    df = clean_teams(df)
+    # 3.1) Format data
+    df = format_data.format_column_date(df)
+    df = format_data.posesion_balon(df)
+    df = format_data.clean_teams(df)  # no funca o si?
+
+    # 3.2) Construct data
+    df = construct_data.equipo_ganador(df)  # Determino columna "equipo_ganador" segun goles_loc y goles_vis
+    df = construct_data.numero_lesionados(df)  # Determino numero de lesionados segun cantidad de lesionados
+    df = construct_data.dif_gol(df, n_ult_part=N_ULT_PART)  # Determino diferencia de gol de cada uno  de los equipos en los ultimos partidos
+    df = construct_data.diferencia_col(df, col1='dig_gol_ult_part_loc', col2='dig_gol_ult_part_vis', nombre_nueva_col='dif_gol')
+    df = construct_data.historial_entre_si(df, n_part=N_ULT_PART)  # Determino columna "historial_entre_si"
+    l_variables_a_prom = ['posesion', 'remates', 'remates_a_puerta', 'tarjetas_amarillas', 'faltas', 'pases', 'pases_comp', 'offsides', 'ataques', 'ataques_pelig']
+    for var in l_variables_a_prom:
+        df = construct_data.promedio_ult_partidos(df, n_ult_part=N_ULT_PART, variable=var)
+        nombre_loc , nombre_vis = f'{var}_ult_part_loc',  f'{var}_ult_part_vis'
+        df = construct_data.diferencia_col(df, col1=nombre_loc, col2=nombre_vis , nombre_nueva_col=f'dif_{var}')
+    print(df.head())
+    df.to_excel('/Users/nachomondino/Desktop/df_constructed.xlsx')
+
+    # 3.3) Select data
+    # Elimino variables que no usare en el modelo como id o fecha (la idea es usar todas las posibles)
+    df = df.drop(['id', 'fecha', 'cancha'], axis=1)
+
+    # Remocion de variables redundantes (las de mayor correlacion)
+    df_correlation_matrix = df.drop('equipo_ganador', axis=1).corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
+    df_correlation_matrix.to_excel('/Users/nachomondino/Desktop/correlation_matrix.xlsx')
+    df.drop(['dif_pases', 'dif_pases_comp', 'dif_remates_a_puerta', 'dif_tarjetas_amarillas', 'dif_ataques_pelig'], inplace=True, axis=1)
+
+    # Elimino otras variables no son importantes... (lo hice manual sin algoritmo pero falta algoritmo)
+    # select_data.feature_selection(df)
+    df = df.drop(['dif_faltas', 'dif_offsides', 'dif_ataques_ataques'], axis=1)  # Para red neuronal
+    df.to_excel('/Users/nachomondino/Desktop/df_selected.xlsx')
+
+    # Elimino filas con al menos un NaN?
+    # df = df.dropna(subset=['historial_entre_si']).reset_index()  # Elimina filas con al menos un valor nulo
+
+    # 3.4) Clean data
     df = clean_data.categorize_numeric_columns(df)  # Categorizo columnas numericas
 
-    # 3.2) Integrate data
-    df = integrate_data.derive_winning_team(df)  # Determino columna "equipo_ganador"
-    df = integrate_data.derive_historial_entre_si(df, n_part=5)  # Determino columna "historial_entre_si"
-    df = integrate_data.derive_dif_gol_last_matches(df, n_part=5)  # Determino diferencia de gol de cada uno  de los equipos en los ultimos partidos
-    df = integrate_data.derive_forma_ponderada(df=df, n_part=5)
-    print(df.head())
+    df = df.dropna(subset=['historial_entre_si']).reset_index()  # Elimina filas con al menos un valor nulo
 
-    # Remuevo columnas fecha
-    df.drop('id', inplace=True, axis=1)
-    df.drop('fecha', inplace=True, axis=1)
-    df.drop('goles_loc', inplace=True, axis=1)
-    df.drop('goles_vis', inplace=True, axis=1)
-    df['dif_gol'] = df['dif_gol_loc'] - df['dif_gol_vis']
-    df.drop('dif_gol_loc', inplace=True, axis=1)
-    df.drop('dif_gol_vis', inplace=True, axis=1)
-    print(df.shape)
+    # df = clean_data.balance_dataset(df, var_resp='equipo_ganador')
 
-    # Remuevo ultimos  partidos de cada equipo (pues no puedo calcular bien la forma)
-    df = df.dropna(subset='dif_forma_pond').reset_index(drop=True)  # Son 15 partidos por jornada y saco las ultimas 10 jornadas pues la forma la calculo 10 partidos para atras...
-    print(df.shape)
-    print(df.head)
+    df.to_excel('/Users/nachomondino/Desktop/df_prepared.xlsx')
 
     # 4) MODELING
-    # Corro 100 modelos y promedio resultados
-    N_CORRIDAS = 1
-    l_aciertos, l_prob, l_prob_2 = [], [], []
-    for i in range(N_CORRIDAS):
-        print(" MODELO Nº{} ".format(i).center(120, '#'))
+    # Definicion de variables
+    N_MODELOS = 10
+    l_aciertos = []
 
-        # Balanceo dataset  (Para mi en este proeycto no conviene balancear el dataset. Puesto que es importante que es mas probable ganar de local que de visitante). Sin embargo entiendo que el modelo puede parecer que da bien pero en realidad siempre decir que gana el local...
-        # df = balance_dataset(df_shuf)
+    # Por modelo
+    for i in range(N_MODELOS):
+
+        # Shuffle dataset
+        df = df.sample(frac=1).reset_index(drop=True)
 
         # Separo conjunto de datos en train y test
         df_train, df_test = test_design.separate_train_and_test(df)
 
         # Implemento Naive Bayes
-        df_prob = naive_bayes.train_naive_bayes(df_train)
-        print(df_prob)
+        modelo_nb = naive_bayes.train_naive_bayes(df_train, var_resp='equipo_ganador')
 
         # 5) EVALUACION DEL MODELO
-        df_result = naive_bayes.predict_naive_bayes(df_prob, df_test)
+        df_result = naive_bayes.predict_naive_bayes(modelo_nb, df_test, var_resp='equipo_ganador', col_prob_clase=True)
+        precision = calculate_precision(df_result, var_resp='equipo_ganador')
+        l_aciertos.append(precision)
 
-        n_aciertos = 0
-        prob_certeza = 0
-        for i in range(len(df_result)):
-            y_real = df_result.loc[i, 'y_real']
-            y_pred = df_result.loc[i, 'y_pred']
-
-            if y_real == y_pred:
-                n_aciertos += 1
-                prob_certeza += df_result.loc[i, 'y_pred_prob']
-
-        # Guardo resultados del modelo
-        l_aciertos.append(n_aciertos / len(df_result) * 100)
-        l_prob.append(prob_certeza / n_aciertos)
-        l_prob_2.append(sum(df_result['y_pred_prob']) / len(df_result))
-        print(l_aciertos)
-        print(l_prob)
-        print(l_prob_2)
-
-    print("El % de aciertos es {:.3f}%".format(sum(l_aciertos) / N_CORRIDAS))
-    print("La probabilidad de certeza promedio en los aciertos es {:.3f}%".format(sum(l_prob) / N_CORRIDAS))
-    print("La probabilidad de certeza promedio es {:.3f}%".format(sum(l_prob_2) / N_CORRIDAS))
-
-    # MATRIZ DE CONFUSION
-    confusion_matrix = metrics.confusion_matrix(df_result['y_real'], df_result['y_pred'])
-    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,
-                                                display_labels=["Local", "Empate", "Visitante"])
-    cm_display.plot()
-    plt.show()
-
-    df.to_excel("./df_results_naive.xlsx")
-
+    print(f"Max: {max(l_aciertos)} Min: {min(l_aciertos)} Prom: {sum(l_aciertos) / len(l_aciertos)}")
 
 main()
