e5a505e34e18f2b71658297dea97c1dd0e52c306
nachomondino1 <nachomondino1@gmail.com>
2023-05-04 12:07:33 -0300

    Intente implementar feature selection pero aun no pude Quite la regresion lineal

--- data_preparation/select_data.py
+++ data_preparation/select_data.py
@@ -1,34 +1,45 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import RandomForestClassifier
 
-# IMPLEMENTAR CORRELACION DE VARIABLES PARA DESCARTAR LAS REDUNDANTES
-# cargar los datos en un dataframe
-path_read = '/Users/nachomondino/Desktop/df_constructed.xlsx'  # '/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/flashscore/liga_argentina_historico_2_prueba.xlsx'
-path_export = '/Users/nachomondino/Desktop/correlation_matrix_2.xlsx'  # '/Users/nachomondino/Desktop/correlation_matrix.xlsx'
+def feature_selection(df):  # Es sin variables categoricas
 
-df = pd.read_excel(path_read)
+    X, y = df.drop('equipo_ganador', axis=1), df['equipo_ganador']
 
-'''
-# calcular la matriz de correlación
-df_correlation_matrix = df.corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
+    # Ajusta un modelo de bosque aleatorio a los datos
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf.fit(X, y)
 
-# mostrar la matriz de correlación
-df_correlation_matrix.to_excel(path_export)  # Cambiar la ruta del archivo
-'''
+    # Evalúa la importancia de las características
+    importances = rf.feature_importances_
+
+    # Selecciona las características con importancia por encima de un umbral
+    threshold = 0.05
+    selector = SelectFromModel(rf, threshold=threshold, prefit=True)
+    X_new = selector.transform(X)
+
+    # Muestra la importancia de las características seleccionadas
+    support = selector.get_support()
+    for feature, importance, supported in zip(X.columns, importances, support):
+        if supported:
+            print(f"{feature}: {importance:.3f} (selected)")
+        else:
+            print(f"{feature}: {importance:.3f}")
+
+    # Ajusta un nuevo modelo de bosque aleatorio solo con las características seleccionadas
+    rf_new = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf_new.fit(X_new, y)
 
 
-# IMPLEMENTAR REGRESION -->  seleccionar las variables más importantes.
-# separar las variables predictoras (X) y la variable objetivo (y)
-df = df.drop(['id', 'fecha', 'equipo_loc', 'equipo_vis', 'arbitro', 'cancha', 'dt_loc', 'dt_vis', 'l_jug_lesionados_loc', 'l_jug_lesionados_vis', 'historial_entre_si'], axis=1)
-df = df.dropna(axis=0, how='any')  # Borro partidos (filas) con al menos un nan
+def main():
+    df = pd.read_excel('/Users/nachomondino/Desktop/df_constructed.xlsx', index_col=0)
 
-X = df.drop('equipo_ganador', axis=1)
-y = df['equipo_ganador']
-l_col = X.columns
+    df_correlation_matrix = df.drop('equipo_ganador', axis=1).corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
+    df_correlation_matrix.to_excel('/Users/nachomondino/Desktop/correlation_matrix.xlsx')
 
-# ajustar el modelo de regresión lineal
-model = LinearRegression().fit(X, y)
+    df.drop(['dif_pases', 'dif_pases_comp', 'dif_remates_a_puerta', 'dif_tarjetas_amarillas', 'dif_ataques_pelig'], inplace=True, axis=1)
 
-importance = model.coef_
-for i,v in enumerate(importance):
-    print('Variable %0s: %.5f' % (l_col[i],v))
+
+    # feature_selection(df)
+
+# main()
