713bc9d21d70d17ea6ed1378cbe1924da7abb6bf
camondino <84874180+camondino@users.noreply.github.com>
2023-05-04 16:25:14 -0300

    cambie la ruta del archivo porque era una vieja
    estoy usando el archivo raw

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -1,15 +1,21 @@
 import pandas as pd
-from dspy.data_preparation import clean_data
-from dspy.modeling import naive_bayes, test_design
-from evaluation.evaluation import calculate_precision
+# from dspy.data_preparation import clean_data
+# from dspy.modeling import naive_bayes, test_design
+# from evaluation.evaluation import calculate_precision
 
+
+# Levanto dataset
+df = pd.read_excel('data_understanding/collect_data/liga_argentina_historico.xlsx', index_col=0)
+print(df.head())
+
+# Vamos a usar modelos de librerias independientes de la libreria que hizo Nacho
 def main():
     # Definicion de variables
     N_MODELOS = 10
     l_aciertos = []
 
     # Levanto dataset
-    df = pd.read_excel('/Users/nachomondino/Desktop/df_prepared.xlsx', index_col=0)
+    df = pd.read_excel('data_understanding/collect_data/liga_argentina_historico.xlsx', index_col=0)
     print(df.head())
 
     # Balanceo dataset y elimino filas con historial=NaN
@@ -22,16 +28,16 @@ def main():
         df = df.sample(frac=1).reset_index(drop=True)
 
         # Separo conjunto de datos en train y test
-        df_train, df_test = test_design.separate_train_and_test(df)
+        # df_train, df_test = test_design.separate_train_and_test(df)
 
         # Implemento Naive Bayes
-        modelo_nb = naive_bayes.train_naive_bayes(df_train, var_resp='equipo_ganador')
+        # modelo_nb = naive_bayes.train_naive_bayes(df_train, var_resp='equipo_ganador')
 
         # 5) EVALUACION DEL MODELO
-        df_result = naive_bayes.predict_naive_bayes(modelo_nb, df_test, var_resp='equipo_ganador', col_prob_clase=True)
-        precision = calculate_precision(df_result, var_resp='equipo_ganador')
-        l_aciertos.append(precision)
+        # df_result = naive_bayes.predict_naive_bayes(modelo_nb, df_test, var_resp='equipo_ganador', col_prob_clase=True)
+        # precision = calculate_precision(df_result, var_resp='equipo_ganador')
+        # l_aciertos.append(precision)
 
-    print(f"Max: {max(l_aciertos)} Min: {min(l_aciertos)} Prom: {sum(l_aciertos)/len(l_aciertos)}")
+    # print(f"Max: {max(l_aciertos)} Min: {min(l_aciertos)} Prom: {sum(l_aciertos)/len(l_aciertos)}")
 
 main()
