2ad50986e41ee5939ce52362e65a367a37b3ff43
nachomondino1 <nachomondino1@gmail.com>
2023-05-07 16:42:01 -0300

    - Cambie nombre de modeling.py a build_model.py - Pase todo a main.py

---
+++ modeling/build_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from dspy.data_preparation import clean_data
+from dspy.modeling import test_design
+from dspy.modeling.supervised_learning import naive_bayes
+from modeling.asses_model import calculate_precision
+
+def xgboost():
+    pass
+
+def arbol_decision():
+    pass
+
+def main():
+    pass
+
+# main()
