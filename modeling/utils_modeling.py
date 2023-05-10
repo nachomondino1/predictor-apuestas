5e306d37b86a85cd167ee7c221b158984b22ff3d
camondino <84874180+camondino@users.noreply.github.com>
2023-05-10 15:29:51 -0300

    Termine de ordenar el codigo en dos archivos

--- modeling/utils_modeling.py
+++ modeling/utils_modeling.py
@@ -206,25 +206,3 @@ def hiper_optimos(df, modelo_a_entrenar='arbol'):
 
     # results = grid_search.cv_results_
     # return results 
-
-
-###### DATASET #######
-# Levanto dataset
-df, classes_names = load_dataset_and_clean(path='data_preparation/df_prepared.xlsx')
-
-###### HIPERPARAMETROS #######
-num_folds = 10 # Cantidad de divisiones de validacion cruzada
-max_depth_tree = 25 # Profundidad del arbol
-number_tress_in_forest = 50 # Cantidad de arboles en el bosque de Random Forest
-
-# Eje x: K de validacion cruzada & Eje y: Precision
-
-# Eje x: Max profundidad & Eje y: Precision
-
-# Eje x: Cantidad de arboles & Eje y: Precision
-
-###### MODELOS #######
-metricas = train_and_test(num_folds, 'xgboost', df, max_depth_tree, number_tress_in_forest, plot_tree_bool = False, plot_conf_matrix = True)
-print(metricas) #  arbol xgboost random_forest
-
-# hiper_optimos(df, modelo_a_entrenar="xgboost")
