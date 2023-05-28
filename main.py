7f20314fbff83e73ed55ba5d27d5a3978ff2911e
nachomondino1 <nachomondino1@gmail.com>
2023-05-28 20:14:04 -0300

    Se me ocurrio hacer clases en main.py por cada etapa de manera de poder decidir que task correr y asi poder hacer pruebas probando ≠ procesamientos

--- main.py
+++ main.py
@@ -2,27 +2,26 @@
 import pandas as pd
 import time
 
-from data_preparation import construct_data, format_data, select_data
+# Data preparation
+from data_preparation import format_data, integrate_data, construct_data, select_data, clean_data
 from dspy.data_preparation import clean_data
 from sklearn.preprocessing import LabelEncoder
 from imblearn.over_sampling import RandomOverSampler
 
+# Modeling
+from modeling import copia_build_model, asses_model
 from sklearn.utils import shuffle
 from dspy.modeling import test_design
 from dspy.modeling.supervised_learning import naive_bayes
 
 
-from modeling.asses_model import calculate_precision, confusion_matrix, calculate_ROI
-
-
-################################################## 3) DATA PREPARATION #################################################
 class DataPreparation:
 
     def __init__(self, df_part, df_jug):
         self.df_part = df_part
         self.df_jug = df_jug
 
-    def format(self):
+    def format_data(self):
 
         # Convierto posesion de string a integer
         self.df_part = format_data.remove_percent_sign(self.df_part)
@@ -34,108 +33,117 @@ class DataPreparation:
         # Remuevo strings adicionales en los nombres de los equipos
         self.df_part = format_data.remove_strings_from_teams(self.df_part)
 
-    def integrate(self):
+    def integrate_data(self):
 
         # Definicion de variables
         l_var = ['l_jug_tit_loc', 'l_jug_tit_vis', 'l_jug_sup_loc', 'l_jug_sup_vis', 'l_jug_ausentes_loc',
                  'l_jug_ausentes_vis']
 
         # Preparo las columnas con texto como los nombres de equipos y los nombre de jugadores
-        self.df_part = prepare_text_columns(self.df_part)
-        self.df_jug = prepare_text_columns(self.df_jug)
+        self.df_part = integrate_data.prepare_text_columns(self.df_part)
+        self.df_jug = integrate_data.prepare_text_columns(self.df_jug)
 
         # Separo columnas listas en multiples columnas (NO VA A SER NECESARIO CUANDO DESDE LA MISMA EXTRACCION EXTRAIGA VARIAS COLUMNAS...)
         for var in l_var:
-            self.df_part = separate_lists_in_columns(self.df_part, var)
+            self.df_part = integrate_data.separate_lists_in_columns(self.df_part, var)
 
         # Integro datasets
         for var in l_var:
-            self.df = search_player_data(self.df_part, self.df_jug, var)
+            self.df = integrate_data.search_player_data(self.df_part, self.df_jug, var)
 
         self.df.to_excel('./df_integrated.xlsx', index=False)
 
-    def construct(self, N_ULT_PART = 5):
+    def construct_data(self, N_ULT_PART = 5):
 
          # Ordeno por campo 'fecha'
          # df = df.sort_values(by='fecha', ascending=False, ignore_index=True)
 
-         # Construct data
-         self.df = construct_data.equipo_ganador(self.df)  # Determino columna "equipo_ganador" segun goles_loc y goles_vis
-         self.df = construct_data.historial_entre_si_segun_localia(self.df, n_ult_part=N_ULT_PART)
-
-         # No genero dif_gol porque tiene alta correlacion (0.9) con dif_forma
-         df = construct_data.promedio_dif_gol_ult_part(df, n_ult_part=N_ULT_PART)  # Determino diferencia de gol de cada uno  de los equipos en los ultimos partidos
-
-         df = construct_data.forma_reciente(df, n_part=N_ULT_PART)
-
          l_variables_a_prom = ['posesion', 'remates', 'remates_a_puerta', 'tarjetas_amarillas', 'faltas', 'pases',
                                'pases_comp', 'offsides', 'ataques', 'ataques_pelig']
+
+         # Construct data
+         self.df = construct_data.equipo_ganador(self.df)  # Determino columna "equipo_ganador" segun goles_loc y goles_vis
+
+         self.df = construct_data.historial_entre_si_segun_localia(self.df, n_ult_part=3)
+
+         self.df = construct_data.promedio_dif_gol_ult_part(self.df, n_ult_part=N_ULT_PART)  # Determino diferencia de gol de cada uno  de los equipos en los ultimos partidos
+
+         self.df = construct_data.forma_reciente(self.df, n_part=N_ULT_PART)  # df = derive_forma_ponderada(df, n_part=N_ULT_PART)
+
+         # Calculo diferencias para las variables promedio de los jugadores
+         self.df['dif_rat_tit'] = self.df['l_jug_tit_loc_prom_rat'] - self.df['l_jug_tit_vis_prom_rat']
+         self.df['dif_edad_tit'] = self.df['l_jug_tit_loc_prom_edad'] - self.df['l_jug_tit_vis_prom_edad']
+         self.df['dif_alt_tit'] = self.df['l_jug_tit_loc_prom_alt'] - self.df['l_jug_tit_vis_prom_alt']
+         self.df['dif_rat_sup'] = self.df['l_jug_sup_loc_prom_rat'] - self.df['l_jug_sup_vis_prom_rat']
+         self.df['dif_edad_sup'] = self.df['l_jug_sup_loc_prom_edad'] - self.df['l_jug_sup_vis_prom_edad']
+         self.df['dif_alt_sup'] = self.df['l_jug_sup_loc_prom_alt'] - self.df['l_jug_sup_vis_prom_alt']
+
          for var in l_variables_a_prom:
-             df = construct_data.promedio_ult_partidos(df, n_ult_part=N_ULT_PART, variable=var)
+             self.df = construct_data.promedio_ult_partidos(self.df, n_ult_part=N_ULT_PART, variable=var)
 
-         return df
+         self.df = construct_data.n_dias_ult_partido(self.df)
+         # df = convert_odds_to_prob(df)
+         # df = numero_lesionados(df)  # Determino numero de lesionados segun cantidad de lesionados
 
-    def select(df):
+    def select_data(self):
 
          # Caro
          # df = df.drop(['historial_entre_si', 'fecha', 'odds_loc', 'odds_emp', 'odds_vis', 'es_copa'], axis = 1)
 
          # Elimino variables que no usare en el modelo como id o fecha (la idea es usar todas las posibles)
-         df = df.drop(['id', 'fecha', 'cancha'], axis=1)
+         self.df = self.df.drop(['id', 'fecha', 'cancha'], axis=1)
 
          # Remocion de variables redundantes (las de mayor correlacion)
-         df_correlation_matrix = df.drop('equipo_ganador', axis=1).corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
+         df_correlation_matrix = self.df.drop('equipo_ganador', axis=1).corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
          df_correlation_matrix.to_excel('/Users/nachomondino/Desktop/correlation_matrix.xlsx')
-         df.drop(['dif_pases_segun_ult_part', 'dif_pases_comp_segun_ult_part', 'dif_remates_a_puerta_segun_ult_part',
+         self.df.drop(['dif_pases_segun_ult_part', 'dif_pases_comp_segun_ult_part', 'dif_remates_a_puerta_segun_ult_part',
                   'dif_tarjetas_amarillas_segun_ult_part', 'dif_ataques_segun_ult_part', 'dif_ataques_pelig_segun_ult_part'],
                  inplace=True, axis=1)
 
          # Elimino otras variables no son importantes... (lo hice manual sin algoritmo pero falta algoritmo)
          # select_data.feature_selection(df)
-         df = df.drop(['dif_faltas_segun_ult_part', 'dif_offsides_segun_ult_part'], axis=1)  # Para red neuronal
-         return df
+         self.df = self.df.drop(['dif_faltas_segun_ult_part', 'dif_offsides_segun_ult_part'], axis=1)  # Para red neuronal
 
-    def clean(df):
+    def clean_data(self):
          # Categorizo columnas numericas
          # df = clean_data.categorize_numeric_columns(df)
 
          # Remover NaN values
-         df = df.dropna()  # inplace=True  # df = df.dropna(subset=['dif_forma']).reset_index()  # Elimina filas con al menos un valor nulo en dif_gol (primeros partidos)
+         self.df = self.df.dropna()  # inplace=True  # df = df.dropna(subset=['dif_forma']).reset_index()  # Elimina filas con al menos un valor nulo en dif_gol (primeros partidos)
 
          # Eliminacion de outliers
-         return df
 
-##################################################### 4) MODELING #####################################################
+
 class Modeling:
 
-    def __init__(self, df):
+    def __init__(self, df, var_resp):
         self.df = df
+        self.var_resp = var_resp
 
-    def generate_test(df, var_resp):
+    def generate_test_design(self):
 
          # Definicion de variables
          le = LabelEncoder()
          oversampler = RandomOverSampler()
 
          # Shuffle dataset
-         df_mezclado = pd.DataFrame(shuffle(df))  # df = df.sample(frac=1).reset_index(drop=True)
+         self.df = pd.DataFrame(shuffle(self.df))  # df = df.sample(frac=1).reset_index(drop=True)
 
          # Convertir variables categoricas string a categoricas numericas
-         for col in df_mezclado.select_dtypes(include=['object']).columns:
-             df_mezclado[col] = le.fit_transform(df_mezclado[col])
+         for col in self.df.select_dtypes(include=['object']).columns:
+             self.df[col] = le.fit_transform(self.df[col])
 
          # Balanceamos segun variable respuesta
-         df = clean_data.balance_dataset(df, var_resp=var_resp)
+         self.df = clean_data.balance_dataset(self.df, var_resp=self.var_resp)
          # X, y = df_mezclado.drop(var_resp, axis=1), df_mezclado[var_resp]
          # X_bal, y_bal = oversampler.fit_resample(X, y)
 
          # Separo conjunto de datos en train y test
-         df_train, df_test = test_design.separate_train_and_test(df, porc_corte=0.8)
-         df_train = df_train.drop(['odds_loc', 'odds_emp', 'odds_vis'], axis=1)
-         print(df_train.shape, df_test.shape)
-         return df_train, df_test  # retorno df_train y df_test?
+         self.df_train, self.df_test = test_design.separate_train_and_test(self.df, porc_corte=0.8)
+         self.df_train = self.df_train.drop(['odds_loc', 'odds_emp', 'odds_vis'], axis=1)
+         print(self.df_train.shape, self.df_test.shape)
 
-    def build_model(df_train, df_test, var_resp):
+    def build_model(self):
 
         # Definicion de variables
         warnings.filterwarnings("ignore")
@@ -156,7 +164,7 @@ class Modeling:
 
             # Entreno modelo
             print(f" Modelo: {str(modelo)[:str(modelo).find('(')]} ".center(120, '#'))
-            model, cv_accuracy, test_accuracy = build_model(df_train, df_test, var_resp, modelo, best_params=True, k=5) # model = DecisionTreeClassifier()  # model2 = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], max_depth=grid_search.best_params_['max_depth'], random_state=42)
+            model, cv_accuracy, test_accuracy = copia_build_model.build_model(self.df_train, self.df_test, self.var_resp, modelo, best_params=True, k=5) # model = DecisionTreeClassifier()  # model2 = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], max_depth=grid_search.best_params_['max_depth'], random_state=42)
 
             # Si es el mejor modelo hasta aqui
             if test_accuracy > best_acurracy:  # GUARDAR MAS METRICAS? HAGO EL ASSESS MODEL ACA?
@@ -164,10 +172,11 @@ class Modeling:
                 best_acurracy = test_accuracy
                 best_model = model
 
-        print(f"\nEl mejor modelo es: {best_model}")
-        return best_model, df_result   #df_result y_real e y_pred
+        print(f"\nEl mejor modelo es: {self.best_model}")
+        self.best_model = best_model
+        self.df_result #df_result y_real e y_pred
 
-    def assess_model(model, df_test, var_resp):
+    def assess_model(self):  # model, df_test, var_resp):
 
         # Hago prediccion aca? y calculo metricas?
 
@@ -187,6 +196,9 @@ def main():  # La idea es poner toda el camino de los datos aqui...
     modeling = True
     var_resp = 'equipo_ganador'
 
+    # Hiperparametros
+    N_ULT_PART = 5
+
     if data_prep is True:
 
         # Levanto datasets
@@ -195,20 +207,15 @@ def main():  # La idea es poner toda el camino de los datos aqui...
 
         prepare = DataPreparation(df_part, df_jug)
 
-        # 3.1 Format data
-        prepare.format()
+        # Preparo el dataset para el analisis
+        prepare.format_data()
+        prepare.integrate_data()
+        prepare.construct_data(N_ULT_PART=N_ULT_PART)
+        prepare.select_data()
+        prepare.clean_data()
 
-        # 3.2 Integrate data
-        df = integrate(df_part, df_jug)
-
-        # 3.3 Construct data
-        df = construct(df, N_ULT_PART=5)
-
-        # 3.4 Select data
-        df = select(df)
-
-        # 3.5 Clean data
-        df = clean(df)
+        # Obtengo el dataset ya preparado
+        df = prepare.df
 
     else:
          # Levanto dataset ya preparado
@@ -217,13 +224,12 @@ def main():  # La idea es poner toda el camino de los datos aqui...
 
     if modeling is True:
 
-        # 4.2 Generate test design
-        df_train, df_test = generate_test(df, var_resp=var_resp)
+        # Definicion de variables
+        modeler = Modeling(df, 'equipo_ganador')
 
-        # 4.3 Build model
-        model = build_model(df_train, df_test, var_resp=var_resp)
-
-        # 4.4 Assess model
-        assess_model(model, df_test, var_resp=var_resp)
+        # Analizo los datos
+        modeler.generate_test_design()
+        modeler.build_model()
+        modeler.assess_model()
 
 main()
