41550cb7589dda9822ad4f589be0f30a0bf449f7
nachomondino1 <nachomondino1@gmail.com>
2023-05-29 11:40:17 -0300

    Confirmé la decision de usar clases por etapa.
    Dejo de definir a los dataframes resultantes de cada task como atributo sino que ahora lo retorno. Entre otras cosas, gano flexibilidad

--- main.py
+++ main.py
@@ -9,154 +9,158 @@ from data_understanding.collect_data import scraper_sofifa, scraper_flashscore
 from data_preparation import format_data, integrate_data, construct_data, select_data, clean_data
 from dspy.data_preparation import clean_data
 # from sklearn.preprocessing import LabelEncoder
-# from imblearn.over_sampling import RandomOverSampler
 
 # Modeling
 # from modeling import copia_build_model, asses_model
 # from sklearn.utils import shuffle
 # from dspy.modeling import test_design
 # from dspy.modeling.supervised_learning import naive_bayes
+# from imblearn.over_sampling import RandomOverSampler
 
 
+# Por ahi es al pedo la clase, ver si conviene y justificar.
 class DataPreparation:
 
-    def __init__(self, df_part, df_jug, var_resp):
-        self.df_part = df_part
-        self.df_jug = df_jug
+    def __init__(self, var_resp):
         self.var_resp = var_resp
 
-    def format_data(self):
+    def format_data(self, df_part, df_jug):
 
         # Convierto posesion de string a integer
-        self.df_part = format_data.remove_percent_sign(self.df_part)
+        df_part = format_data.remove_percent_sign(df_part)
 
         # Convierto fecha de string a datetime
-        self.df_part = format_data.transform_date_column(self.df_part, string_format='%d.%m.%Y %H:%M')  # Fundamental para poder ordenar el df por 'fecha'
-        self.df_jug = format_data.transform_date_column(self.df_jug, string_format='%b %d, %Y')
+        df_part = format_data.transform_date_column(df_part, string_format='%d.%m.%Y %H:%M')  # Fundamental para poder ordenar el df por 'fecha'
+        df_jug = format_data.transform_date_column(df_jug, string_format='%b %d, %Y')
 
         # Remuevo strings adicionales en los nombres de los equipos
-        self.df_part = format_data.remove_strings_from_teams(self.df_part)
+        df_part = format_data.remove_strings_from_teams(df_part)
 
-        self.df_part.to_excel('./data_preparation/data/df_part_formated.xlsx', index=False)
-        self.df_jug.to_excel('./data_preparation/data/df_jug_formated.xlsx', index=False)
+        df_part.to_excel('./data_preparation/data/df_part_formated.xlsx', index=False)
+        df_jug.to_excel('./data_preparation/data/df_jug_formated.xlsx', index=False)
 
-    def integrate_data(self):
+        return df_part, df_jug
+
+    def integrate_data(self, df_part, df_jug):
 
         # Definicion de variables
         l_var = ['l_jug_tit_loc', 'l_jug_tit_vis', 'l_jug_sup_loc', 'l_jug_sup_vis', 'l_jug_ausentes_loc',
                  'l_jug_ausentes_vis']
 
         # Preparo las columnas con texto como los nombres de equipos y los nombre de jugadores
-        self.df_part = integrate_data.prepare_text_columns(self.df_part)
-        self.df_jug = integrate_data.prepare_text_columns(self.df_jug)
+        df_part = integrate_data.prepare_text_columns(df_part)
+        df_jug = integrate_data.prepare_text_columns(df_jug)
 
         # Separo columnas listas en multiples columnas (NO VA A SER NECESARIO CUANDO DESDE LA MISMA EXTRACCION EXTRAIGA VARIAS COLUMNAS...)
         for var in l_var:
-            self.df_part = integrate_data.separate_lists_in_columns(self.df_part, var)
+            df_part = integrate_data.separate_lists_in_columns(df_part, var)
 
         # Integro datasets
         for var in l_var:
-            self.df = integrate_data.search_player_data(self.df_part, self.df_jug, var)
+            df_integrated = integrate_data.search_player_data(df_part, df_jug, var)
 
-        self.df.to_excel('./data_preparation/data/df_integrated.xlsx', index=False)
+        df_integrated.to_excel('./data_preparation/data/df_integrated.xlsx', index=False)
+        return df_integrated
 
-    def construct_data(self, N_ULT_PART = 5):
+    def construct_data(self, df, N_ULT_PART = 5):
 
-         # Ordeno por campo 'fecha'
-         # df = df.sort_values(by='fecha', ascending=False, ignore_index=True)
+        # Ordeno por campo 'fecha'
+        # df = df.sort_values(by='fecha', ascending=False, ignore_index=True)
 
-         l_variables_a_prom = ['posesion', 'remates', 'remates_a_puerta', 'tarjetas_amarillas', 'faltas', 'pases',
+        l_variables_a_prom = ['posesion', 'remates', 'remates_a_puerta', 'tarjetas_amarillas', 'faltas', 'pases',
                                'pases_comp', 'offsides', 'ataques', 'ataques_pelig']
 
-         # Construct data
-         self.df = construct_data.equipo_ganador(self.df)  # Determino columna "equipo_ganador" segun goles_loc y goles_vis
+        # Construct data
+        df = construct_data.equipo_ganador(df)  # Determino columna "equipo_ganador" segun goles_loc y goles_vis
 
-         self.df = construct_data.historial_entre_si_segun_localia(self.df, n_ult_part=3)
+        df = construct_data.historial_entre_si_segun_localia(df, n_ult_part=int(N_ULT_PART/2))
 
-         self.df = construct_data.promedio_dif_gol_ult_part(self.df, n_ult_part=N_ULT_PART)  # Determino diferencia de gol de cada uno  de los equipos en los ultimos partidos
+        df = construct_data.promedio_dif_gol_ult_part(df, n_ult_part=N_ULT_PART)  # Determino diferencia de gol de cada uno  de los equipos en los ultimos partidos
 
-         self.df = construct_data.forma_reciente(self.df, n_part=N_ULT_PART)  # df = derive_forma_ponderada(df, n_part=N_ULT_PART)
+        df = construct_data.forma_reciente(df, n_part=N_ULT_PART)  # df = derive_forma_ponderada(df, n_part=N_ULT_PART)
 
-         # Calculo diferencias para las variables promedio de los jugadores
-         self.df['dif_rat_tit'] = self.df['l_jug_tit_loc_prom_rat'] - self.df['l_jug_tit_vis_prom_rat']
-         self.df['dif_edad_tit'] = self.df['l_jug_tit_loc_prom_edad'] - self.df['l_jug_tit_vis_prom_edad']
-         self.df['dif_alt_tit'] = self.df['l_jug_tit_loc_prom_alt'] - self.df['l_jug_tit_vis_prom_alt']
-         self.df['dif_rat_sup'] = self.df['l_jug_sup_loc_prom_rat'] - self.df['l_jug_sup_vis_prom_rat']
-         self.df['dif_edad_sup'] = self.df['l_jug_sup_loc_prom_edad'] - self.df['l_jug_sup_vis_prom_edad']
-         self.df['dif_alt_sup'] = self.df['l_jug_sup_loc_prom_alt'] - self.df['l_jug_sup_vis_prom_alt']
+        # Calculo diferencias para las variables promedio de los jugadores
+        df['dif_rat_tit'] = df['l_jug_tit_loc_prom_rat'] - df['l_jug_tit_vis_prom_rat']
+        df['dif_edad_tit'] = df['l_jug_tit_loc_prom_edad'] - df['l_jug_tit_vis_prom_edad']
+        df['dif_alt_tit'] = df['l_jug_tit_loc_prom_alt'] - df['l_jug_tit_vis_prom_alt']
+        df['dif_rat_sup'] = df['l_jug_sup_loc_prom_rat'] - df['l_jug_sup_vis_prom_rat']
+        df['dif_edad_sup'] = df['l_jug_sup_loc_prom_edad'] - df['l_jug_sup_vis_prom_edad']
+        df['dif_alt_sup'] = df['l_jug_sup_loc_prom_alt'] - df['l_jug_sup_vis_prom_alt']
 
-         for var in l_variables_a_prom:
-             self.df = construct_data.promedio_ult_partidos(self.df, n_ult_part=N_ULT_PART, variable=var)
+        for var in l_variables_a_prom:
+            df = construct_data.promedio_ult_partidos(df, n_ult_part=N_ULT_PART, variable=var)
 
-         self.df = construct_data.n_dias_ult_partido(self.df)
-         # df = convert_odds_to_prob(df)
-         # df = numero_lesionados(df)  # Determino numero de lesionados segun cantidad de lesionados
+        df = construct_data.n_dias_ult_partido(df)
+        # df = convert_odds_to_prob(df)
+        # df = numero_lesionados(df)  # Determino numero de lesionados segun cantidad de lesionados
 
-         self.df.to_excel('./data_preparation/data/df_constructed.xlsx', index=False)
+        df.to_excel('./data_preparation/data/df_constructed.xlsx', index=False)
+        return df
 
-    def select_data(self):
+    def select_data(self, df):
 
-         # Caro
-         # df = df.drop(['historial_entre_si', 'fecha', 'odds_loc', 'odds_emp', 'odds_vis', 'es_copa'], axis = 1)
+        # Caro
+        # df = df.drop(['historial_entre_si', 'fecha', 'odds_loc', 'odds_emp', 'odds_vis', 'es_copa'], axis = 1)
 
-         # Elimino variables que no usare en el modelo como id o fecha (la idea es usar todas las posibles)
-         self.df = self.df.drop(['id', 'fecha', 'cancha'], axis=1)
+        # Elimino variables que no usare en el modelo como id o fecha (la idea es usar todas las posibles)
+        df = df.drop(['id', 'fecha', 'cancha'], axis=1)
 
-         # Remocion de variables redundantes (las de mayor correlacion)
-         df_correlation_matrix = self.df.drop(self.var_resp, axis=1).corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
-         df_correlation_matrix.to_excel('/Users/nachomondino/Desktop/correlation_matrix.xlsx')
-         self.df.drop(['dif_pases_segun_ult_part', 'dif_pases_comp_segun_ult_part', 'dif_remates_a_puerta_segun_ult_part',
-                  'dif_tarjetas_amarillas_segun_ult_part', 'dif_ataques_segun_ult_part', 'dif_ataques_pelig_segun_ult_part'],
-                 inplace=True, axis=1)
+        # Remocion de variables redundantes (las de mayor correlacion)
+        df_correlation_matrix = df.drop(self.var_resp, axis=1).corr()  # OJO que no tiene en cuenta las variables categoricas... y si quiero tenerlas en cuenta como "equipo ganador"
+        df_correlation_matrix.to_excel('/Users/nachomondino/Desktop/correlation_matrix.xlsx')
+        df.drop(['dif_pases_segun_ult_part', 'dif_pases_comp_segun_ult_part', 'dif_remates_a_puerta_segun_ult_part', 'dif_tarjetas_amarillas_segun_ult_part', 'dif_ataques_segun_ult_part', 'dif_ataques_pelig_segun_ult_part'], inplace=True, axis=1)
 
-         # Elimino otras variables no son importantes... (lo hice manual sin algoritmo pero falta algoritmo)
-         # select_data.feature_selection(df)
-         self.df = self.df.drop(['dif_faltas_segun_ult_part', 'dif_offsides_segun_ult_part'], axis=1)  # Para red neuronal
+        # Elimino otras variables no son importantes... (lo hice manual sin algoritmo pero falta algoritmo)
+        # select_data.feature_selection(df)
+        df = df.drop(['dif_faltas_segun_ult_part', 'dif_offsides_segun_ult_part'], axis=1)  # Para red neuronal
 
-         self.df.to_excel('./data_preparation/data/df_selected.xlsx', index=False)
+        df.to_excel('./data_preparation/data/df_selected.xlsx', index=False)
+        return df
 
-    def clean_data(self):
-         # Categorizo columnas numericas
-         # df = clean_data.categorize_numeric_columns(df)
+    def clean_data(self, df):
 
-         # Remover NaN values
-         self.df = self.df.dropna()  # inplace=True  # df = df.dropna(subset=['dif_forma']).reset_index()  # Elimina filas con al menos un valor nulo en dif_gol (primeros partidos)
+        # Categorizo columnas numericas
+        # df = clean_data.categorize_numeric_columns(df)
 
-         # Eliminacion de outliers
+        # Remover NaN values
+        df = df.dropna()  # inplace=True  # df = df.dropna(subset=['dif_forma']).reset_index()  # Elimina filas con al menos un valor nulo en dif_gol (primeros partidos)
 
-         self.df.to_excel('./data_preparation/data/df_cleaned.xlsx', index=False)
+        # Eliminacion de outliers
 
+        # Convertir variables categoricas string a categoricas numericas
+        le = LabelEncoder()
+        for col in df.select_dtypes(include=['object']).columns:
+            df[col] = le.fit_transform(df[col])
+
+        df.to_excel('./data_preparation/data/df_cleaned.xlsx', index=False)
+        return df
 
 class Modeling:
 
-    def __init__(self, df, var_resp):
-        self.df = df
+    def __init__(self, var_resp):
         self.var_resp = var_resp
 
-    def generate_test_design(self):
+    def generate_test_design(self, df):
 
-         # Definicion de variables
-         le = LabelEncoder()
-         oversampler = RandomOverSampler()
+        # Definicion de variables
+        oversampler = RandomOverSampler()
 
-         # Shuffle dataset
-         self.df = pd.DataFrame(shuffle(self.df))  # df = df.sample(frac=1).reset_index(drop=True)
+        # Shuffle dataset
+        df = pd.DataFrame(shuffle(df))  # df = df.sample(frac=1).reset_index(drop=True)
 
-         # Convertir variables categoricas string a categoricas numericas
-         for col in self.df.select_dtypes(include=['object']).columns:
-             self.df[col] = le.fit_transform(self.df[col])
+        # Balanceamos segun variable respuesta
+        df = clean_data.balance_dataset(df, var_resp=self.var_resp)
+        # X, y = df_mezclado.drop(var_resp, axis=1), df_mezclado[var_resp]
+        # X_bal, y_bal = oversampler.fit_resample(X, y)
 
-         # Balanceamos segun variable respuesta
-         self.df = clean_data.balance_dataset(self.df, var_resp=self.var_resp)
-         # X, y = df_mezclado.drop(var_resp, axis=1), df_mezclado[var_resp]
-         # X_bal, y_bal = oversampler.fit_resample(X, y)
+        # Separo conjunto de datos en train y test
+        df_train, df_test = test_design.separate_train_and_test(df, porc_corte=0.8)
+        df_train = df_train.drop(['odds_loc', 'odds_emp', 'odds_vis'], axis=1)
+        print(df_train.shape, df_test.shape)
 
-         # Separo conjunto de datos en train y test
-         self.df_train, self.df_test = test_design.separate_train_and_test(self.df, porc_corte=0.8)
-         self.df_train = self.df_train.drop(['odds_loc', 'odds_emp', 'odds_vis'], axis=1)
-         print(self.df_train.shape, self.df_test.shape)
+        return df_train, df_test
 
-    def build_model(self):
+    def build_model(self, df_train, df_test):  # que argumentos? l_modelos, best_params?, k?
 
         # Definicion de variables
         warnings.filterwarnings("ignore")
@@ -177,7 +181,7 @@ class Modeling:
 
             # Entreno modelo
             print(f" Modelo: {str(modelo)[:str(modelo).find('(')]} ".center(120, '#'))
-            model, cv_accuracy, test_accuracy = copia_build_model.build_model(self.df_train, self.df_test, self.var_resp, modelo, best_params=True, k=5) # model = DecisionTreeClassifier()  # model2 = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], max_depth=grid_search.best_params_['max_depth'], random_state=42)
+            model, cv_accuracy, test_accuracy = copia_build_model.build_model(df_train, df_test, self.var_resp, modelo, best_params=True, k=5) # model = DecisionTreeClassifier()  # model2 = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], max_depth=grid_search.best_params_['max_depth'], random_state=42)
 
             # Si es el mejor modelo hasta aqui
             if test_accuracy > best_acurracy:  # GUARDAR MAS METRICAS? HAGO EL ASSESS MODEL ACA?
@@ -185,20 +189,22 @@ class Modeling:
                 best_acurracy = test_accuracy
                 best_model = model
 
-        print(f"\nEl mejor modelo es: {self.best_model}")
-        self.best_model = best_model
-        self.df_result #df_result y_real e y_pred
+        print(f"\nEl mejor modelo es: {best_model}")
+        return best_model, df_result #df_result y_real e y_pred
 
-    def assess_model(self):  # model, df_test, var_resp):
+
+    def assess_model(self, model, df_test):
 
         # Hago prediccion aca? y calculo metricas?
 
+        '''
         df_result = naive_bayes.predict_naive_bayes(modelo_nb, df_test, var_resp=var_resp, con_prob=True)
         df_result.to_excel('/Users/nachomondino/Desktop/df_result.xlsx')
         precision = calculate_precision(df_result, var_resp=var_resp, var_pred=var_pred)
         roi = calculate_ROI(df_result, var_resp=var_resp, var_pred=var_pred)
-
         # confusion_matrix(df_result, var_resp=var_resp, var_pred=var_pred)
+        '''
+
         return precision, roi
 
 
@@ -206,8 +212,8 @@ def main():  # La idea es poner toda el camino de los datos aqui...
 
     # Definicion de variables
     data_unders = False
-    data_prep = True
-    modeling = False
+    data_prep = False
+    modeling = True
     var_resp = 'equipo_ganador'
 
     # Hiperparametros
@@ -225,17 +231,14 @@ def main():  # La idea es poner toda el camino de los datos aqui...
 
     if data_prep is True:
 
-        prepare = DataPreparation(df_part, df_jug, var_resp)
+        prepare = DataPreparation(var_resp)
 
         # Preparo el dataset para el analisis
-        prepare.format_data()
-        prepare.integrate_data()
-        # prepare.construct_data(N_ULT_PART=N_ULT_PART)
-        # prepare.select_data()
-        # prepare.clean_data()
-
-        # Obtengo el dataset ya preparado
-        df = prepare.df  # cuando solo corro format: AttributeError: 'DataPreparation' object has no attribute 'df'
+        df_part, df_jug = prepare.format_data(df_part, df_jug)
+        df = prepare.integrate_data(df_part, df_jug)
+        df = prepare.construct_data(df, N_ULT_PART=N_ULT_PART)
+        df = prepare.select_data(df)
+        df = prepare.clean_data(df)
 
     else:
          # Levanto dataset ya preparado
@@ -245,11 +248,11 @@ def main():  # La idea es poner toda el camino de los datos aqui...
     if modeling is True:
 
         # Definicion de variables
-        modeler = Modeling(df, 'equipo_ganador')
+        modeler = Modeling(var_resp)
 
         # Analizo los datos
-        modeler.generate_test_design()
-        modeler.build_model()
-        modeler.assess_model()
+        df_train, df_test = modeler.generate_test_design(df)
+        best_model, df_result = modeler.build_model(df_train, df_test)
+        precision, roi = modeler.assess_model(best_model, df_result)
 
 main()
