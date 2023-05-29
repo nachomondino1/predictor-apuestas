362d37ccc4c6a0d2b98b9d662f1aa64f062888f4
nachomondino1 <nachomondino1@gmail.com>
2023-05-29 18:37:44 -0300

    Dejo la opcion de extraer el nombre completo como comentario y vuelvo a extraer el nombre corto. Esto es porque tuve problemas para integrar los datasets con nombre completo en uno y nombre corto en otro.

--- data_understanding/collect_data/scraper_sofifa.py
+++ data_understanding/collect_data/scraper_sofifa.py
@@ -78,7 +78,7 @@ def extract_sofifa():
                             d_data = {}
                             d_data['id_jugador'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@data-col="pi"]', text=True)
                             d_data['fecha'] = fecha
-                            d_data['nombre'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@class="col-name"]/a', attribute="aria-label")
+                            d_data['nombre'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@class="col-name"]/a/div[@class="ellipsis"]', text=True)  # l. gonzalez pirez # Nombre completo: d_data['nombre'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@class="col-name"]/a', attribute="aria-label")
                             d_data['edad'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@data-col="ae"]', text=True)
                             d_data['altura'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@data-col="hi"]', text=True)
                             d_data['pie_habil'] = crawler.extract_tag(tag_inicial=tag, xpath='.//td[@data-col="pf"]', text=True)
