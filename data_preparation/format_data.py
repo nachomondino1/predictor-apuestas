9ce9c4a5945847c5af432af529326d0415f4e5d5
nachomondino1 <nachomondino1@gmail.com>
2023-06-02 16:16:29 -0300

    Arregle extraccion de cuotas que extraia "Cuotas retiradas por la casa de apuestas." en vez del valor de la cuota. Por ello, quite la funcion que convertir odds en float de format_data.py.

--- data_preparation/format_data.py
+++ data_preparation/format_data.py
@@ -43,20 +43,6 @@ def convert_valor_mercado_to_int(df):
     df['valor_mercado'] = df['valor_mercado'].apply(convertir_valor_mercado)
     return df
 
-def convert_odds_to_float(df):  # El nuevo dataframe tuvo falla en la extraccion de algunas cuotas (lo cual ya lo tendria arreglado para proximas extracciones) y hace que el dtype sea object...
-    """
-    Transformo posesion de string a float
-    :param df: Dataframe. Con columnas 'posesion_loc' y 'posesion_vis' donde la posesion se interpreta como string. Por
-    ejemplo '65%'.
-    :return: Dataframe. Con columna 'fecha' interpretada como float. Por ejemplo, '0.65'
-    """
-    l_var = ['odds_loc', 'odds_emp', 'odds_vis']
-    for var in l_var:
-        print(f"Antes: {df[var]}")
-        df[var] = df[var].replace("Cuotas retiradas por la casa de apuestas.", None).apply(lambda x: float(x) if isinstance(x, str) else np.nan)
-        print(f"Despues: {df[var]}")
-    return df
-
 def prueba():
     # Levanto datasets
     df_part = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/data_seg/entidad_partido_argentina.xlsx')
@@ -66,7 +52,6 @@ def prueba():
     df_part = convert_fecha_to_datetime(df_part, string_format='%d.%m.%Y %H:%M')
     df_part = convert_posesion_to_int(df_part)
     df_part['es_copa'] = df_part['es_copa'].replace(True, 1).replace(False, 0)
-    df_part = convert_odds_to_float(df_part)  # Extrajo mal cuotas en algunos partidos...
 
     # Entidad jugador: fecha de string a datetime y convierto valor de mercado en entero
     df_jug = convert_fecha_to_datetime(df_jug, string_format='%b %d, %Y')
@@ -75,4 +60,4 @@ def prueba():
     df_part.to_excel('/Users/nachomondino/Desktop/df_part_formated.xlsx', index=False)
     df_jug.to_excel('/Users/nachomondino/Desktop/df_jug_formated.xlsx', index=False)
 
-prueba()
+# prueba()
--- data_understanding/collect_data/scraper_flashscore_bad_fields.py
+++ data_understanding/collect_data/scraper_flashscore_bad_fields.py
@@ -1,72 +1,83 @@
 # Importo librerias
 import pandas as pd
-import datetime
-import time
-import random
-import warnings
 from dspy.data_understanding.web_scraping.selenium import Crawler
 
 
-def extract_cuota(crawler, SEC_WAIT, i):  # Probar funcion... NO ESTA VERIFICADA...  # Puedo volver a la anterior, solo fallaron 41 cuotas por "Cuotas retiradas por la casa de apuestas."
-
-    # Busco odd suponiendo que cambio durante el partido
-    try:
-        text_with_odds = crawler.extract_tag(xpath=f'.//div[@class="cellWrapper"][{i}]', attribute='title', sec_wait=SEC_WAIT)  # 3.00 » 2.25
-        odds = float(text_with_odds.split('»')[0].strip())  # 3.00
-        return odds
-
-    # Si la odd no cambio durante el partido, o bien, aparece "Cuotas retiradas por la casa de apuestas."
-    except ValueError:  # could not convert string to float:
-        try:
-            odds_str = crawler.extract_tag(xpath=f'.//div[@class="cellWrapper"][{i}]//span[@class="oddsValueInner"]', text=True, sec_wait=SEC_WAIT)
-            odds = float(odds_str)
-            return odds
-
-        except ValueError:  # could not convert string to float
-            print("Fallo extraccion de la cuota")
-            return None
-
-def extract_flashscore():
+def extract_bad_fields_flashscore():
     """
     It contains all the extraction logic, i.e. it directs the bot on WHEN to perform each action. First initialize the
     driver, then enter the page, then accept cookies and so on.
     """
     # DEFINCION DE PARAMETROS & VARIABLES
-    warnings.filterwarnings("ignore")  # /Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/scraper_flashscore.py:175: FutureWarning: In a future version, object-dtype columns with all-bool values will not be included in reductions with bool_only=True. Explicitly cast to bool dtype instead. df_part = pd.concat([df_part, pd.DataFrame(d_nueva_fila, index=[0])])
     SEC_WAIT, SEC_WAIT_LONG = 0.2, 1.5
     crawler = Crawler(headless=True, path=None) # Creo objeto de clase CrawlerActions()
+    l_var = ['odds_loc', 'odds_emp', 'odds_vis']
 
+    # Levanto dataset de partidos ya extraidos
     df_part = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/data_seg/entidad_partido_argentina.xlsx')
 
     # Por partido
     for i in range(len(df_part)):
-        print(f"Partido Nº{i+1}")
-        id = df_part.loc[i, 'id']
 
-        # Reinicio diccionario en el que guardar la nueva fila
-        d_nueva_fila = {}
+        print(f" Partido Nº{i + 1} ".center(120, "#"))
 
-        # Ingreso a pagina de informacion del partido
-        crawler.driver.get(f'https://www.flashscore.es/partido/{id}/#/resumen-del-partido')
+        # Por variable
+        for var in l_var:
 
-        # Si existe la seccion "Cuotas pre-partido", extraigo cuotas de Bet365
-        if crawler.extract_tag(xpath='.//div[@class="oddsRow"]') is not None:  # No sirve en algunos partidos en los que existe la seccion de las cuotas pero no hay valores...
+            # Convierto str a float
+            try:
+                float(df_part.loc[i, var])
 
-            d_nueva_fila['odds_loc'] = extract_cuota(crawler, SEC_WAIT, i=1)
-            d_nueva_fila['odds_emp'] = extract_cuota(crawler, SEC_WAIT, i=2)
-            d_nueva_fila['odds_vis'] = extract_cuota(crawler, SEC_WAIT, i=3)
-            print(id, d_nueva_fila['odds_loc'], d_nueva_fila['odds_emp'], d_nueva_fila['odds_vis'])
+            # Si falla la conversion a float (es un str)
+            except ValueError:
 
-        # GUARDADO DE DATOS EN DATAFRAME
-        df_part.loc[i, 'odds_loc'] = d_nueva_fila['odds_loc']
-        df_part.loc[i, 'odds_emp'] = d_nueva_fila['odds_emp']
-        df_part.loc[i, 'odds_vis'] = d_nueva_fila['odds_vis']
+                # Vuelvo a buscar la cuota
+                id = df_part.loc[i, 'id']
+
+                # Reinicio diccionario en el que guardar la nueva fila
+                d_nueva_fila = {'odds_loc': None, 'odds_emp': None, 'odds_vis': None}
+
+                # Ingreso a pagina de informacion del partido
+                crawler.driver.get(f'https://www.flashscore.es/partido/{id}/#/resumen-del-partido')
+
+                # Si existe la seccion "Cuotas pre-partido", extraigo cuotas de Bet365
+                if crawler.extract_tag(xpath='.//div[@class="oddsRow"]') is not None:  # No sirve en algunos partidos en los que existe la seccion de las cuotas pero no hay valores...
+
+                    d_nueva_fila['odds_loc'] = extract_cuota(crawler, SEC_WAIT, i=1)
+                    d_nueva_fila['odds_emp'] = extract_cuota(crawler, SEC_WAIT, i=2)
+                    d_nueva_fila['odds_vis'] = extract_cuota(crawler, SEC_WAIT, i=3)
+
+                print(f" Partido Nº{i + 1} ".center(120, "#"))
+                print(f'{id} \nOdds_loc: {df_part.loc[i, "odds_loc"]} -> {d_nueva_fila["odds_loc"]} '
+                      f'\nOdds emp: {df_part.loc[i, "odds_emp"]} -> {d_nueva_fila["odds_emp"]}'
+                      f'\nOdds vis: {df_part.loc[i, "odds_vis"]} -> {d_nueva_fila["odds_vis"]}')
+
+                # GUARDADO DE DATOS EN DATAFRAME
+                df_part.loc[i, 'odds_loc'] = d_nueva_fila['odds_loc']
+                df_part.loc[i, 'odds_emp'] = d_nueva_fila['odds_emp']
+                df_part.loc[i, 'odds_vis'] = d_nueva_fila['odds_vis']
+                break
 
     # Guardado de archivo excel en computadora
+    df_part['odds_loc'] = df_part['odds_loc'].apply(lambda x: float(x))
+    df_part['odds_emp'] = df_part['odds_emp'].apply(lambda x: float(x))
+    df_part['odds_vis'] = df_part['odds_vis'].apply(lambda x: float(x))
+
     df_part.to_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/collect_data/data_seg/entidad_partido_argentina_prueba.xlsx', index=False)
 
     # Finalizada la extraccion, cierro el web browser automático
     crawler.driver.close()
 
+def extract_cuota(crawler, SEC_WAIT, i):  # Puedo volver a la anterior, solo fallaron 41 cuotas por "Cuotas retiradas por la casa de apuestas."
+    try:
+        odds_str = crawler.extract_tag(xpath=f'.//div[@class="cellWrapper"][{i}]', attribute='title', sec_wait=SEC_WAIT)  # 3.00 » 2.25
+        cuota = float(odds_str.split('»')[0].strip())  # 3.00
+    except ValueError:
+        try:
+            odds_str = crawler.extract_tag(xpath=f'.//div[@class="cellWrapper"][{i}]//span[@class="oddsValueInner"]', text=True, sec_wait=SEC_WAIT)
+            cuota = float(odds_str)
+        except (ValueError, TypeError):
+            cuota = None
+    return cuota
 
-extract_flashscore()
+extract_bad_fields_flashscore()
