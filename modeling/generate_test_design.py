c06d5a51d3dcd3d5721b724a1902ad02310f1bf1
nachomondino1 <nachomondino1@gmail.com>
2023-06-19 19:33:40 -0300

    Quite el shuffle y lo puse en main.py dada la importancia que tiene tal shuffle.
    Quite notas y las pase a la documentacion.

--- modeling/generate_test_design.py
+++ modeling/generate_test_design.py
@@ -9,19 +9,13 @@ from dspy.data_preparation import clean_data
 def balance_dataset(X, y, type):
 
     # Balanceamos segun variable respuesta
-    if type == 'over':  # Tiene overfitting. CUIDADO: 'over'usa odds para balancear... No hay forma de evitar esas columnas para balancear...
-        # Genera odds que no son reales al agregar registros... eso afecta el calculo del roi...
+    if type == 'over':  # Genera overfitting
         oversampler = RandomOverSampler()  #
         X_bal, y_bal = oversampler.fit_resample(X, y)
 
-    elif type == 'under':  # Tiene underfitting...
+    elif type == 'under':  # Genera underfitting si no hago shuffle despues
         undersampler = RandomUnderSampler()  # Funciona igual que mi funcion pero no cambia dtypes, por lo que, no arroja errores
         X_bal, y_bal = undersampler.fit_resample(X, y)
-        print(X_bal.shape)
-
-        # Uso mi propia funcion porque me llama la atencion que de tan mal con la libreria...
-        # X_bal, y_bal = clean_data.balance_dataset(X, y)
-        # print(X_bal.shape)
 
     elif type == "over_and_under":  # funciona mal, es lo que mismo que 'over'  --> como sabe que tiene que hacer over de la clase minoritaria y under de la clase mayoritaria?
         pipeline = make_pipeline(SMOTE(), RandomUnderSampler())
@@ -31,8 +25,4 @@ def balance_dataset(X, y, type):
         print("El tipo ingresado para balancear los datos no es una opcion")
         raise ValueError(f"Error: El tipo de balanceo '{type}', no es una opcion")
 
-    # Shuffle y borro index --> fundamental para evitar problemas en CV por los folds?
-    df_bal = pd.concat([X_bal, y_bal], axis=1)
-    df_bal = df_bal.sample(frac=1).reset_index(drop=True)  # Para evitar que queden misma clase en un fold de CV?
-    X_bal, y_bal = df_bal.drop(columns=[y.name]), df_bal[y.name]
     return X_bal, y_bal
