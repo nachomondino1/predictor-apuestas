2d37c9dfe26ba362d72116c198e88af5a9c1c85d
nachomondino1 <nachomondino1@gmail.com>
2023-07-11 18:57:48 -0300

    En select_best_hiperparameters()  1) retorno , ademas del modelo, los mejores hiperparametros para poder llevar cuenta de los valores que mas toma cada hiper (tal vez es momentaneo)  2) Ajuste algunos hiperparametros de acuerdo a los valores que mas toman.

--- p4_modeling/build_model.py
+++ p4_modeling/build_model.py
@@ -20,66 +20,74 @@ def select_best_hiperparameters(model, X, y, k):
 
     # Definicion de variables
     d_params = {
-        'DecisionTreeClassifier': { # 1292 Nºcomb / min  => 2726 comb = 2,1 min
-            'criterion': ['gini', 'entropy'],
-            'splitter': ['best', 'random'],
-            'max_depth': [None, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44],
+        'DecisionTreeClassifier': {
+            'criterion': ['entropy'],  # 'gini'
+            'splitter': ['random', 'best'],
+            'max_depth': [None, 5, 7, 8, 10, 12, 14],
             'min_samples_split': [2, 5, 10],
             'min_samples_leaf': [1, 2, 4],
-            'max_features': ['auto', 'sqrt', 'log2', None],
-            'random_state': [42]},
-        'RandomForestClassifier': { # 32 Nºcomb / min  => 60 comb = 2 min
+            'max_features': ['auto', None, 'sqrt', 'log2'],
+            # 'random_state': [42]
+        },
+        'RandomForestClassifier': {
             'n_estimators': [50, 150, 200],  # 100
-            'criterion': ['gini', 'entropy'],
-            'max_depth': [5, 7, 8, 10, 15],  # None, 15, 20, 25, 30, 35
+            'criterion': ['entropy'],  # 'gini'
+            'max_depth': [None, 5, 7, 8, 10, 15],
             # 'min_samples_split': [2, 5, 10],
             # 'min_samples_leaf': [1, 2, 4],
-            # 'max_features': ['auto', 'sqrt', 'log2'],
-            'bootstrap': [True, False],
-            'random_state': [42]},
-        'XGBClassifier': { # 61 Nºcomb / min  => 44 comb = 0.7 min
-            'max_depth': [None, 5, 6, 7, 8, 10, 15, 20, 25, 30, 35],
-            'n_estimators': [50, 100, 150, 200]},
-        'LogisticRegression': { # 1300 Nºcomb / min  => 2160 comb = 1,7 min
+            # 'max_features': ['auto', None],  # 'sqrt', 'log2'
+            'bootstrap': [True],  # False
+            # 'random_state': [42]
+        },
+        'XGBClassifier': {
+            'max_depth': [None, 5, 6, 7, 8, 10, 15],
+            'n_estimators': [50, 100, 150, 200]
+        },
+        'LogisticRegression': {
             'penalty': [None, 'l1', 'l2', 'elasticnet'],
             'C': [0.1, 1.0, 10.0],
-            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+            'solver': ['lbfgs', 'liblinear', 'newton-cg'],  # , 'sag', 'saga'
             'max_iter': [100, 500, 1000],
             'multi_class': ['ovr', 'multinomial', 'auto'],
-            'fit_intercept': [True, False],
-            'class_weight': [None, 'balanced'],
-            'random_state': [42]},
-        'SVC': { # 240 Nºcomb / min  => 48 comb = 0,2 min
-            # Hay al menos un hiperparametro que hace que tarde años... --> Los que no son: kernel, degree, decision_func_shape
-            'C': [0.1, 0.5, 1.0], #  10.0 # gana siempre 0.1?
-            'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],  # gana siempre linear, a veces poly
-            'degree': [2, 3, 4, 5],  # 1.2 min con 96 hiper # gana siempre 2?
-            'gamma': ['scale', 'auto'],  # puede ser
-            'coef0': [0.0, 0.1, 1.0],  # puede ser
+            # 'fit_intercept': [True, False],
+            # 'class_weight': [None, 'balanced'],
+            # 'random_state': [42]
+        },
+        'SVC': {
+            'C': [0.1, 0.5, 1.0], # gana siempre 0.1?
+            # 'kernel': ['linear', 'poly'],  # 'rbf', 'sigmoid'  # gana siempre linear, a veces poly
+            'degree': [1, 2, 3],  # 4, 5
+            'gamma': ['scale', 'auto'],
+            'coef0': [0.0, 0.1, 1.0],
             'shrinking': [True, False],
             'probability': [True, False],
             'tol': [1e-3, 1e-4, 1e-5],
-            'decision_function_shape': ['ovo', 'ovr'], # gana siempre 'ovo'
-            'random_state': [42]},
-        'MLPClassifier': { # 106 Nºcomb / min  => 360 comb = 3,4 min
-            'activation': ['identity', 'sigmoid','logistic', 'tanh', 'relu'],  # 'identity', 'sigmoid'
-            'solver': ['lbfgs', 'sgd', 'adam'],
-            'learning_rate': ['constant', 'learning_rate', 'invscaling', 'adaptive'],  # 'learning_rate'
-            'max_iter': [200, 300],  # 200
-            'hidden_layer_sizes': [(64), (128), (64, 32)]},
-        'GradientBoostingClassifier': { # 4 Nºcomb / min  => 45 comb = 11,25 min
+            'decision_function_shape': ['ovo'],  # 'ovr'
+            # 'random_state': [42]
+        },
+        'MLPClassifier': {
+            'activation': ['logistic',  'relu', 'tanh', 'identity'],  # 'sigmoid'
+            'solver': ['sgd', 'lbfgs', 'adam'],
+            'learning_rate': ['constant', 'adaptive', 'invscaling'],  # 'learning_rate',
+            'max_iter': [200, 300],
+            'hidden_layer_sizes': [(64), (128), (64, 32)]
+        },
+        'GradientBoostingClassifier': {
             'learning_rate': [0.1, 0.05, 0.01],
-            'n_estimators': [100, 200, 300],
-            'max_depth': [None, 5, 7, 20, 30]},  # None, 30
+            'n_estimators': [100, 300],  # 200
+            'max_depth': [5, 7, 10]  # None, 30
+        },
         'PCA': {
             'n_components': [2, 5, 10],
             'whiten': [True, False],
             'svd_solver': ['auto', 'full', 'randomized'],
-            'iterated_power': [1, 2, 3]}
+            'iterated_power': [1, 2, 3]
+        }
     }
 
     # Obtengo el nombre del modelo para poder buscar sus hiperparametros
     model_name = str(model)[:str(model).find('(')]
+    # print(f"Hiperparametros a probar: {d_params[model_name]}")
 
     # Crear el objeto GridSearchCV
     grid_search = GridSearchCV(model, param_grid=d_params[model_name], cv=k)
@@ -90,12 +98,11 @@ def select_best_hiperparameters(model, X, y, k):
     # Obtener los mejores hiperparámetros y el modelo con dichos hiperparametros
     best_params = grid_search.best_params_
     model = grid_search.best_estimator_
-    # print(f"Mejores hiperparametros: {best_params}")
-    print(f"Mejores hiperparametros: {model}")
+    # print(f"Mejores hiperparametros: {model}")  # print(f"Mejores hiperparametros: {best_params}")
 
     end = time.time()
     print(f"Seleccion de hiperparametros optimos en {(end - start) / 60:.1f} minutos")
-    return model
+    return model, best_params
 
 def manual_cross_validation(model, X_train, y_train, k=5):  # Funciona igual que la libreria (podria utilizar la libreria si quiero o no) # antes recibia X e y --> lo saque para hacer la division en train y test en generate test design
     """
