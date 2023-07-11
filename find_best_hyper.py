43a3647f76aae2db424908da9b40c52518c1f914
nachomondino1 <nachomondino1@gmail.com>
2023-07-11 18:54:26 -0300

    Dejo de usar DecisionTreeClassifier puesto que es un subconjunto de Random.
    Mejore la definicion de los parametros por task
    
    Construi df_hiper para guardar los mejores hiperparametros de cada modelo en cada iteracion. Para guiarme en que valores son los que normalmente tienen.
    
    Integro datos solo si ya no tengo el dataset integrado.

--- find_best_hyper.py
+++ find_best_hyper.py
@@ -14,80 +14,114 @@ def find_best_hiperparameters(var_resp, var_pred, pais):
 
     # Definicion de variables
     df_res = pd.DataFrame()
+    df_hiper = pd.DataFrame(columns=['modelo', 'hiper', 'value', 'cuenta'])
     best_accuracy = 0.0
-    l_modelos = [DecisionTreeClassifier(), RandomForestClassifier(), xgb.XGBClassifier(), LogisticRegression(),
-                 SVC(), MLPClassifier(), GradientBoostingClassifier()]
+    l_modelos = [RandomForestClassifier(), xgb.XGBClassifier(), LogisticRegression(), SVC(), MLPClassifier(), GradientBoostingClassifier()]
     dp = DataPreparation(var_resp, pais)
     mo = Modeling(var_resp, var_pred, pais)  # Creo objeto de clase Modeling
 
     # Definicion de hiperparametros
-    param_integrate = {'fill_with_fs': [True, False], 'n_dias_player_data': [365]}
-    param_construct = {'n_dias': [30], 'n_anios_historial': [2]}
-    param_clean = {'thr_nan_col': [0.2, None]}
-    param_select = {'thr_corr': [0.5, None], 'thr_fs': [0.2, 0.1, 0.3]}  # 'thr_nan_col': [0.2, 0.5, None]
-    param_mod = {'test_val_size': [0.2], 'test_size': [0.5], 'fill_na': [None, 'ml'],
-                 'bal_type': [None, 'under', 'over'], 'k': [5]}
+    d_params = {
+        'integrate': {
+            'fill_with_fs': [True, False],  # Mas adelante, pruebo a ver si mejora rellenando datos...
+            'n_dias_player_data': [365, 180]
+        },
+        'construct': {
+            'n_dias': [30, 60],
+            'n_anios_historial': [2]
+        },
+        'clean': {
+            'thr_nan_col': [0.2, None]
+        },
+        'select': {
+            'thr_corr': [0.7, 0.5, None],
+            'thr_fs': [0.2, 0.35]
+        },
+        'modeling': {
+            'test_val_size': [0.2],
+            'test_size': [0.5],
+            'fill_na': [None, 'ml'],
+            'bal_type': [None, 'under'],  # 'over' (tiene overfitting)
+            'k': [5]
+        }
+    }
 
     # Imprimo largo de iteraciones
-    n_iter = define_n_iterations(l_dicts=[param_construct, param_select, param_mod])
+    n_iter = define_n_iterations(d_params)
     print(f"Numero de iteraciones totales: {n_iter}")
 
-    for x, param_values_1 in enumerate(product(*param_integrate.values()), start=1):
+    # Por combinacion de parametros de integrate_data
+    for x, param_values_1 in enumerate(product(*d_params['integrate'].values()), start=1):
 
         # Asigno valor a cada hiperpametro
         fill_with_fs, n_dias_player_data = param_values_1[0], param_values_1[1]
+        print(f" Iter Nº {x} ".center(120, "#"))
         print(f'Hiper integrate --> fill_with_fs: {fill_with_fs} ; n_dias_player_data: {n_dias_player_data}')
 
         # Integro datos
-        df = dp.integrate_data(df_part, df_jug_part, df_jug, fill_data_with_flashcore=fill_with_fs, n_dias_player_data=n_dias_player_data)
+        # Pruebo a levantar dataset ya integrado
+        try:
+            df = pd.read_excel(
+                f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_integrated_{fill_with_fs}_{n_dias_player_data}.xlsx')
+
+        except FileNotFoundError:
+            # Levanto datasets formateado
+            df_part = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_part_formated.xlsx')
+            df_jug_part = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p2_data_understanding/data/{pais}/df_jug_part.xlsx')
+            df_jug = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_jug_formated.xlsx')
+
+            # Integro datos
+            df = dp.integrate_data(df_part, df_jug_part, df_jug, fill_data_with_flashcore=fill_with_fs, n_dias_player_data=n_dias_player_data)
+            df.to_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_integrated_{fill_with_fs}_{n_dias_player_data}.xlsx', index=False)
 
         # Por combinacion de parametros de construct_data
-        for i, param_values_2 in enumerate(product(*param_construct.values()), start=1):
+        for i, param_values_2 in enumerate(product(*d_params['construct'].values()), start=1):
 
             # Asigno valor a cada hiperpametro
             n_dias, n_anios_historial = param_values_2[0], param_values_2[1]
-            print(f" Iteracion Nº {i} ".center(120, "#"))
+            print(f" Iter Nº {x}.{i} ".center(120, "#"))
             print(f'Hiper construct --> n_dias: {n_dias} ; n_anios_historial: {n_anios_historial}')
 
             # Pruebo a levantar dataset ya construido
             try:
-                df = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_preparation/data/{pais}/df_constructed_{n_dias}_{n_anios_historial}.xlsx')
-
+                df = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_constructed_{n_dias}_{n_anios_historial}.xlsx')
             except:
-                # Levanto dataset integrado (es el mismo siempre)
-                df = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_preparation/data/argentina/df_integrated.xlsx')
-
                 # Construyo datos
                 df = dp.construct_data(df, n_dias=n_dias, n_anios_historial=n_anios_historial)
-                df.to_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_preparation/data/{pais}/df_constructed_{n_dias}_{n_anios_historial}.xlsx', index=False)
+                df.to_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_constructed_{n_dias}_{n_anios_historial}.xlsx', index=False)
 
-            for m, param_values_3 in enumerate(product(*param_clean.values()), start=1):
+            # Por combinacion de parametros de clean_data
+            for m, param_values_3 in enumerate(product(*d_params['clean'].values()), start=1):
 
                 # Asigno valor a cada hiperpametro
                 thr_nan_col = param_values_3[0]
+                print(f" Iter Nº {x}.{i}.{m} ".center(120, "#"))
+                print(f"Hiper clean --> thr_nan_col: {thr_nan_col}")
 
                 # Limpio datos
                 df = dp.clean_data(df, thr_nan_col=thr_nan_col, export=False)
-                print(f"Hiper clean --> thr_nan_col: {thr_nan_col}")
 
                 # Por combinacion de parametros de select_data
-                for j, param_values_4 in enumerate(product(*param_select.values()), start=1):
+                for j, param_values_4 in enumerate(product(*d_params['select'].values()), start=1):
 
                     # Asigno valor a cada hiperpametro
-                    thr_nan_col, thr_corr, thr_fs = param_values_4[0], param_values_4[1], param_values_4[2]
-                    print(f" Iteracion Nº {i}.{j} ".center(120, "+"))
+                    thr_corr, thr_fs = param_values_4[0], param_values_4[1]
+                    print(f" Iter Nº {x}.{i}.{m}.{j} ".center(120, "#"))
                     print(f"Hiper select --> thr_corr: {thr_corr} ; thr_fs: {thr_fs}")
 
                     # Selecciono datos
-                    df_sel = dp.select_data(df, thr_nan_col=thr_nan_col, thr_corr=thr_corr, thr_fs=thr_fs, export=False)
-                    # df_sel.to_excel(f'/Users/nachomondino/Desktop/df_selected_{pais}.xlsx')
+                    df_sel = dp.select_data(df, thr_corr=thr_corr, thr_fs=thr_fs, export=False)
 
                     # Por combinacion de parametros de modeling
-                    for h, param_values_5 in enumerate(product(*param_mod.values()), start=1):
+                    for h, param_values_5 in enumerate(product(*d_params['modeling'].values()), start=1):
 
                         # Asigno valor a cada hiperparametro
                         test_val_size, test_size, fill_na, bal_type, k = param_values_5[0], param_values_5[1], param_values_5[2], param_values_5[3], param_values_5[4]
-                        print(f" Iteracion Nº {i}.{j}.{h} ".center(120, "-"))
+                        print(f" Iter Nº {x}.{i}.{m}.{j}.{h} ".center(120, "#"))
+                        print(f'Hiper integrate --> fill_with_fs: {fill_with_fs} ; n_dias_player_data: {n_dias_player_data}')
+                        print(f'Hiper construct --> n_dias: {n_dias} ; n_anios_historial: {n_anios_historial}')
+                        print(f"Hiper clean --> thr_nan_col: {thr_nan_col}")
+                        print(f"Hiper select --> thr_corr: {thr_corr} ; thr_fs: {thr_fs}")
                         print(f"Hiper modeling --> test_val_size: {test_val_size} ; test_size: {test_size}; fill_na: {fill_na} ; bal_type: {bal_type} ; k: {k}")
 
                         df_models = pd.DataFrame(columns=['model_name', 'model_trained', 'train_cv_accuracy', 'test_accuracy', 'test_recall', 'test_f1_score'])  # Datos del modelo y su precision y roi
@@ -99,11 +133,25 @@ def find_best_hiperparameters(var_resp, var_pred, pais):
                         for modelo in l_modelos:
 
                             # Entreno modelo y evaluo su rendimiento
-                            model_name, model_best_params, cv_accuracy = mo.build_model(modelo, X_val, y_val, X_train, y_train, k)
+                            model_name, model_best_params, best_params, cv_accuracy = mo.build_model(modelo, X_val, y_val, X_train, y_train, k)
                             accuracy, recall, f1 = mo.assess_model(model_best_params, X_test, y_test, export=False)
 
                             # Guardo modelo
                             df_models.loc[len(df_models)] = [model_name, model_best_params, cv_accuracy, accuracy, recall, f1]
+
+                            # Guardo mejores de hiperparametros
+                            for hiper, value in best_params.items():
+
+                                fila_deseada = df_hiper.loc[(df_hiper['modelo'] == model_name) & (df_hiper['hiper'] == hiper) & (df_hiper['value'] == value)]
+
+                                # Si ya tiene cuenta
+                                if len(fila_deseada) > 0:
+                                    df_hiper.loc[fila_deseada.index, 'cuenta'] += 1
+
+                                else:
+                                    # Si no tiene cuenta
+                                    hiper_data = [{'modelo': model_name, 'hiper': hiper, 'value': value, 'cuenta': 1}]
+                                    df_hiper = pd.concat([df_hiper, pd.DataFrame(hiper_data)], axis=0).reset_index(drop=True)
 
                         # Selecciono el mejor modelo
                         idx = df_models['test_accuracy'].idxmax()
@@ -132,6 +180,7 @@ def find_best_hiperparameters(var_resp, var_pred, pais):
                         print(row_data)
                         print(df_res)
                         df_res.to_excel('/Users/nachomondino/Desktop/df_best_hyper_df_mod.xlsx', index=False)
+                        df_hiper.to_excel('/Users/nachomondino/Desktop/df_hiper.xlsx', index=False)
 
                         # Verificar si la precisión actual es la mejor hasta ahora
                         if bm_test_acc > best_accuracy:
@@ -147,7 +196,7 @@ def find_best_hiperparameters(var_resp, var_pred, pais):
     df_res.to_excel('/Users/nachomondino/Desktop/df_best_hyper_df_mod.xlsx', index=False)
     return best_hyperparameters
 
-def define_n_iterations(l_dicts):
+def define_n_iterations(d_params):
     """
     Calcula el numero de iteraciones y el tiempo estimado para terminar
     :param l_dicts: Lista de diccionarios de hiperparametros. (list)
@@ -156,11 +205,10 @@ def define_n_iterations(l_dicts):
     # Cuidado con el nro de iteraciones sobretodo en select. nº comb = producto de posibles comb de cada hiper  EJ: {'thr_corr': [0.5, 0.6, 0.7], 'thr_fs': [0.25, 0.2, 0.15, 0.1, 0.05], 'thr_nan_col': [0.2, 0.5, None]} --> nºcomb = 3x5x3=45
     # n comb totales = 3 x 45 x 6 = 810 --> 90 iteraciones en 17 horas --> 5,35 iter/hora => 810 iteraciones = 151 horas  # n comb totales = 1 x 18 x 6 = 108 --> 90 iteraciones en 17 horas --> 5,35 iter/hora => 216 iteraciones = 151 horas
     n_iter = 1
-    for dict in l_dicts:
-        for key in dict.keys():
-            n_iter *= len(dict[key])
+    for task, d_params_task in d_params.items():
+        for key in d_params_task.keys():
+            n_iter *= len(d_params_task[key])
     return n_iter
-
 
 def main():
     pais, var_resp, var_pred = "argentina_south_america", 'equipo_ganador', 'y_pred'
