e613c9868efb8b063000e3996cbd68812dec2455
nachomondino1 <nachomondino1@gmail.com>
2023-07-13 14:57:47 -0300

    - Implemente set de validacion para seleccionar los hiperparametros optimos de los algoritmos de Machine Learning como RandomForestClassifier(). Al usar menos datos, aumento confiabilidad mediante el uso de un k mas alto.
    - Implemente seleccion de hiperparametros optimos tanto en  lasso como en rfe. Sin embargo, opto por descartar lasso por sus resultados deficientes. En cambio, rfe lo implementare de ahora en adelante.

--- p3_data_preparation/select_data.py
+++ p3_data_preparation/select_data.py
@@ -1,11 +1,12 @@
 # Importo librerias
 import pandas as pd
 import warnings
+from sklearn.model_selection import train_test_split
 from p4_modeling.build_model import select_best_hiperparameters
 from sklearn.feature_selection import SelectKBest, f_classif, chi2  # modelos estadisticos
 from sklearn.feature_selection import f_regression  # via
 from sklearn.feature_selection import RFE  # rfe
-from sklearn.linear_model import LinearRegression  # rfe
+from sklearn.linear_model import LinearRegression, LogisticRegression  # rfe
 from sklearn.linear_model import Lasso  # Lasso
 from sklearn.ensemble import RandomForestClassifier  # Random forest
 from sklearn.preprocessing import scale
@@ -96,7 +97,7 @@ class FeatureSelection():
 
         return df_importance
 
-    def random_forest(self, X, y, k=3, graf=False):
+    def random_forest(self, X, y, k=10, graf=False):
         """
         Calculo de importancia de cada variable segun modelo de random forest.
 
@@ -105,11 +106,14 @@ class FeatureSelection():
         :param graf: Boolean. True para graficar importancia por variable. (bool)
         :return: Dataframe. Importancia por variable. (Dataframe)
         """
+        # Separo en train y val (para que select_best_hiperparameters() no tarde tanto)
+        X_train, X_val, y_train, y_val= train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+
         # Verificar si se deben buscar los mejores hiperparámetros
-        model = select_best_hiperparameters(RandomForestClassifier(), X, y, k=k)  # Tarda puesto que X no es del tamaño de X_val sino que de X_train
+        model, best_params = select_best_hiperparameters(RandomForestClassifier(), X_val, y_val, k=k)  # Tarda puesto que X no es del tamaño de X_val sino que de X_train
 
         # Entrenar el modelo final con todos los datos de entrenamiento
-        model.fit(X, y)
+        model.fit(X_train, y_train)
 
         # Obtengo importancias por variable
         df_importance = pd.DataFrame({'random_forest': model.feature_importances_}, index=X.columns)
@@ -152,18 +156,24 @@ class FeatureSelection():
         """
         # Definicion de variables
         n_features = 1  # Número deseado de características seleccionadas hasta que se eliminan las menos relevantes
-        model = LinearRegression()
+
+        # Separo en train y val
+        X_train, X_val, y_train, y_val= train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+
+        # Busco los mejores hiperparametros para el modelo
+        model, best_params = select_best_hiperparameters(LogisticRegression(), X_val, y_val, k=10)  # Tarda puesto que X no es del tamaño de X_val sino que de X_train
         rfe = RFE(estimator=model, n_features_to_select=n_features)
 
         # Entreno modelo
-        X_selected = rfe.fit_transform(X, y)
+        X_selected = rfe.fit_transform(X_train, y_train)
 
         # Obtengo importancias por variable
         df_importance = pd.DataFrame({'rfe': rfe.ranking_}, index=X.columns)
+        # print("Resultados rfe: \n", df_importance)
 
         # Convierto ranking en importancia (a mayor ranking, menor importancia)
         df_importance['rfe'] = df_importance['rfe'].apply(lambda x: len(X.columns) - x + 1)
-        # print("Resultados rfe: \n", df_importance)
+        # print("Resultados rfe dsp convertir: \n", df_importance)
 
         if graf:
             self.graficar_importancia_atrib(X=df_importance['rfe'], y=df_importance.index)
@@ -179,16 +189,20 @@ class FeatureSelection():
         :param graf: Boolean. True para graficar importancia por variable. (bool)
         :return: Dataframe. Importancia por variable. (Dataframe)
         """
+        # Separo en train y val
+        X_train, X_val, y_train, y_val= train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+
+        # Busco los mejores hiperparametros para el modelo
+        model, best_params = select_best_hiperparameters(Lasso(), X_val, y_val, k=10)  # Tarda puesto que X no es del tamaño de X_val sino que de X_train
+
         # Entreno modelo
-        lasso = Lasso(alpha=0.01)  # con 0.05: 11 variables son cero # 0.15: 14 var # con 0.01: 4 var
-        lasso.fit(X, y)
+        model.fit(X_train, y_train)
 
         # Obtengo importancias por variable
-        df_importance = pd.DataFrame({'lasso': lasso.coef_}, index=X.columns)
+        df_importance = pd.DataFrame({'lasso': model.coef_}, index=X.columns)
 
         # Convierto coeficiente en importancia (a mayor coef en valor abs, mas importancia)
-        df_importance['lasso'] = df_importance['lasso'].apply(lambda x: abs(x))  #x es coef
-        # print("Resultados lasso: \n", df_importance)
+        df_importance['lasso'] = df_importance['lasso'].apply(lambda x: abs(x))  # x es coef
 
         if graf:
             self.graficar_importancia_atrib(X=df_importance['lasso'], y=df_importance.index)
@@ -243,7 +257,7 @@ class FeatureSelection():
         df_normalized['suma_de_imp_norm'] = (df_normalized['suma_de_imp'] - df_normalized['suma_de_imp'].min()) / (df_normalized['suma_de_imp'].max() - df_normalized['suma_de_imp'].min())
         return df_normalized
 
-def select_best_features(df, var_resp, thr_fs, graf=True):
+def select_best_features(df, var_resp, thr_fs, graf=False):
     """
     Selecciona las variables mas importantes para un Dataframe.
 
@@ -256,7 +270,7 @@ def select_best_features(df, var_resp, thr_fs, graf=True):
     """
     # Definicion de variables
     fs = FeatureSelection()
-    graficar = False
+    graficar_cada_metodo = False
 
     # Elimino NaN values puesto que no puedo tener NaN en modelos de ml
     df = df.dropna()  # Es dificil que queden pocos registros porque borro filas y col con muchos nan antes
@@ -267,16 +281,15 @@ def select_best_features(df, var_resp, thr_fs, graf=True):
     print(f"Largo del dataframe antes de fs: {X.shape}")
 
     # Detemino importancia de cada variable para cada modelo
-    df_importance = df_importance.merge(fs.modelos_estadisticos(X, y, graf=graficar), left_index=True, right_index=True)
-    df_importance = df_importance.merge(fs.via(X, y, graf=graficar), left_index=True, right_index=True)
-    df_importance = df_importance.merge(fs.rfe(X, y, graf=graficar), left_index=True, right_index=True) # No me gustan sus resultados
-    df_importance = df_importance.merge(fs.lasso_selection(X, y, graf=graficar), left_index=True, right_index=True)  # No me gustan sus resultados
-    df_importance = df_importance.merge(fs.random_forest(X, y, graf=graficar), left_index=True, right_index=True)
-    df_importance.to_excel('/Users/nachomondino/Desktop/df_importance_prueba.xlsx')
+    df_importance = df_importance.merge(fs.modelos_estadisticos(X, y, graf=graficar_cada_metodo), left_index=True, right_index=True)
+    df_importance = df_importance.merge(fs.via(X, y, graf=graficar_cada_metodo), left_index=True, right_index=True)
+    df_importance = df_importance.merge(fs.random_forest(X, y, graf=graficar_cada_metodo), left_index=True, right_index=True)
+    df_importance = df_importance.merge(fs.rfe(X, y, graf=graficar_cada_metodo), left_index=True, right_index=True)
+    # df_importance.to_excel('/Users/nachomondino/Desktop/df_importance_prueba.xlsx')
 
     # Normalizo importancias para poder sumarlas
     df_normalized = fs.sum_and_normalize_importances(df_importance)
-    df_normalized.to_excel('/Users/nachomondino/Desktop/df_normalized_prueba.xlsx')
+    # df_normalized.to_excel('/Users/nachomondino/Desktop/df_normalized_prueba.xlsx')
 
     # Determino columnas a eliminar por poco importancia
     l_not_important_features = df_normalized.loc[df_normalized['suma_de_imp_norm'] < df_normalized['suma_de_imp_norm'].max() * thr_fs].index.tolist()
@@ -297,23 +310,22 @@ def prueba():
     warnings.filterwarnings('ignore')
 
     # Definicion de hiperparametros
-    thr_nan_col = 0.3  # 0.3
-    thr_corr = 0.5  # Correlacion minima entre dos variables para indicar una alta correlacion [0-1] (siendo 1 correlacion maxima y 0 sin correlacion)
-    thr_fs = 0.15  # Peso minimo de una variable para ser considerada como importante [0-1] (siendo 1 el peso de la variable mas importante y 0 la menos)
+    thr_nan_col = 0.4
+    thr_corr = 0.8  # Correlacion minima entre dos variables para indicar una alta correlacion [0-1] (siendo 1 correlacion maxima y 0 sin correlacion)
+    thr_fs = 0.2  # Peso minimo de una variable para ser considerada como importante [0-1] (siendo 1 el peso de la variable mas importante y 0 la menos)
     export = False
 
     # Levanto dataset de prueba
-    df = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_constructed.xlsx', index_col=0)
+    df = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_constructed_True_60_30_2.xlsx')
 
     # Elimino variables que no usare en el modelo como id o fecha (la idea es usar todas las posibles)
-    df = df.drop(['id_part', 'pais', 'competicion', 'temporada', 'fecha', 'cancha'], axis=1)
-    df = df.drop(['prom_edad_loc', 'prom_edad_vis', 'rating_loc', 'rating_vis', 'posesion_loc', 'posesion_vis', 'remates_loc', 'remates_vis', 'remates_a_puerta_loc', 'remates_a_puerta_vis', 'remates_palos_loc', 'remates_palos_vis', 'remates_fuera_loc', 'remates_fuera_vis', 'remates_block_loc', 'remates_block_vis', 'porc_pases_comp_loc', 'porc_pases_comp_vis', 'pases_loc', 'pases_vis', 'pases_comp_loc', 'pases_comp_vis', 'pases_clave_loc', 'pases_clave_vis', 'amagues_loc', 'amagues_vis', 'duelos_aereos_loc', 'duelos_aereos_vis', 'tackles_loc', 'tackles_vis', 'intercepciones_loc', 'intercepciones_vis', 'corners_loc', 'corners_vis', 'faltas_loc', 'faltas_vis', 'offsides_loc', 'offsides_vis', 'ht_goles_loc', 'ht_goles_vis', 'goles_loc', 'goles_vis', 'dif_goles_loc', 'dif_goles_vis', 'puntos_loc', 'puntos_vis'], axis=1)
+    df = df.drop(['id_part', 'pais', 'competicion', 'temporada', 'fecha', 'cancha', 'es_copa'], axis=1)
 
     # Elimino filas y columnas con alto porcentaje de NaN values
     largo_inicial = len(df)
-    df = df.dropna(subset=['dif_remates_segun_ult_part'], how='any')
+    df = df.dropna(subset=['dif_prom_ult_part_dif_remates'], how='any')
     print(f"Se eliminó el {(largo_inicial-len(df))/largo_inicial*100:.0f}% de filas, quedan {len(df)} filas.")
-    df.to_excel('/Users/nachomondino/Desktop/df_drop_na_prueba.xlsx', index=False)
+    # df.to_excel('/Users/nachomondino/Desktop/df_drop_na_prueba.xlsx', index=False)
 
     if thr_nan_col is not None:
         prop_nan = df.isna().mean()
@@ -334,7 +346,7 @@ def prueba():
         l_not_important_features = select_best_features(df, var_resp, thr_fs=thr_fs, graf=True)
         df = df.drop(l_not_important_features, axis=1)
 
-    print(f"Las siguientes {len(df.columns)} columnas son las seleccionadas: {df.columns}")
+    print(f"Las siguientes {len(df.columns)} columnas son las seleccionadas: {list(df.columns)}")
     df.to_excel('/Users/nachomondino/Desktop/df_selected_prueba.xlsx', index=False)
 
 # Código que se ejecuta solo cuando el archivo se ejecuta directamente
