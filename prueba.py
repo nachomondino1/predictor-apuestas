7103a181e60eaf7657dab653e72da46fa200f31b
nachomondino1 <nachomondino1@gmail.com>
2023-07-18 15:30:09 -0300

    Saque al concetanacion para hacer un archivo aparte

--- prueba.py
+++ prueba.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from data_preparation import format_data
 
-concatenar = False
-
 import itertools
 
 l_ids = [1, 2, 3]
@@ -93,44 +91,3 @@ fecha_dt = datetime.datetime.strptime(fecha_str, '%b %d, %Y')
 print(fecha_dt)
 print(fecha_dt.month)
 '''
-
-
-# concatenar df_seg...
-if concatenar:
-    # Levanto datasets a concatenar
-    df_1 = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/data/argentina/entidad_partido.xlsx')
-    df_2 = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/data/sudamerica/entidad_partido.xlsx')
-    # df_3 = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/data/inglaterra/premier-league_1989_1990_inglaterra.xlsx')
-
-    df_1['goles_loc'] = df_1['goles_loc'].astype(int)
-    df_2['goles_loc'] = df_2['goles_loc'].astype(int)
-
-    # df_1 = format_data.convert_fecha_to_datetime(df_1,string_format='%d.%m.%Y %H:%M')  # ya lo voy a extraer datetime... # Fundamental para poder ordenar el df por 'fecha'
-
-    # Imprimo caracteristicas de cada dataframe a concatenar
-    print(df_1.head(1))
-    print(df_1.shape)
-
-    print(df_2.head(1))
-    print(df_2.shape)
-
-    # print(df_3.head(1))
-    # print(df_3.shape)
-
-    # Concateno dataframes
-    # df_concat = pd.concat([df_1, df_2, df_3], axis=0)
-    df_concat = pd.concat([df_1, df_2], axis=0)
-
-    # # Antes del cambio de tipo
-    # print(df_concat.dtypes)
-    #
-    # # Cambiar el tipo de datos de la columna "goles_loc" a int
-    # df_concat['goles_loc'] = df_concat['goles_loc'].astype(int)
-    #
-    # # Después del cambio de tipo
-    # print(df_concat.dtypes)
-
-    print(df_concat.head(1))
-    print(df_concat.shape)
-
-    df_concat.to_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/data_understanding/data/argentina_sudamerica/entidad_partido.xlsx', index=False)
