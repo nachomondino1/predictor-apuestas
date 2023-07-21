f97ce95ba5ee34f9c2361363fa7e5293346ad4a2
nachomondino1 <nachomondino1@gmail.com>
2023-07-21 19:04:16 -0300

    - Agrego la construccion de variables promedio de sofifa (overall rating y valor de mercado) - Actualizo prueba() segun cambios

--- p3_data_preparation/integrate_data.py
+++ p3_data_preparation/integrate_data.py
@@ -33,21 +33,16 @@ def map_player_to_part(df_jug_part, df_part):  # Agregar calculo de rating y min
     for index, row in df_part.iterrows():
         df_jug_part_filt_1 = df_jug_part[df_jug_part['id_part'] == row['id_part']]
 
-        # Por condicion (home, away)
+        # Por condicion {home, away}
         for condicion in l_condiciones:
             df_jug_part_filt_2 = df_jug_part_filt_1[df_jug_part_filt_1['condicion'] == condicion]
 
-            # Por titularidad (tit, sup)
+            # Por titularidad {tit, sup}
             for titularidad in l_titularidades:
 
                 # Selecciono registros
                 df_jug_part_filt_3 = df_jug_part_filt_2[df_jug_part_filt_2['titularidad'] == titularidad]
                 largo = len(df_jug_part_filt_3)
-                # print(df_jug_part_filt_3)
-                # print(sum(df_jug_part_filt_3['edad']))
-                # print(sum(df_jug_part_filt_3['altura']))
-                # print(sum(df_jug_part_filt_3['prom_rating_ult_part']))
-                # print(sum(df_jug_part_filt_3['sum_min_played_ult_part']))
 
                 # Calculo promedio de edad y altura
                 if largo > 0:
@@ -55,12 +50,17 @@ def map_player_to_part(df_jug_part, df_part):  # Agregar calculo de rating y min
                     prom_alt = df_jug_part_filt_3['altura'].mean()
                     sum_rat = df_jug_part_filt_3['prom_pond_rating_ult_part'].dropna().sum()  #  prom_rat = df_jug_part_filt_3['prom_pond_rating_ult_part'].mean()
                     sum_min_played = df_jug_part_filt_3['sum_min_played_ult_part'].dropna().sum()
+                    prom_overall_rat = df_jug_part_filt_3['overall_rating'].mean()  # si hago suma, tengo que rellenar NaN values con min rating...
+                    prom_valor_mercado = df_jug_part_filt_3['valor_mercado'].mean()
 
                     # Guardo columna en df_part
                     df_part.loc[index, f'prom_edad_{condicion}_{titularidad}'] = prom_edad
                     df_part.loc[index, f'prom_alt_{condicion}_{titularidad}'] = prom_alt
                     df_part.loc[index, f'sum_rat_{condicion}_{titularidad}'] = sum_rat  # df_part.loc[index, f'prom_rat_{condicion}_{titularidad}'] = prom_rat
                     df_part.loc[index, f'sum_min_{condicion}_{titularidad}'] = sum_min_played
+                    df_part.loc[index, f'prom_ov_rat_{condicion}_{titularidad}'] = prom_overall_rat
+                    df_part.loc[index, f'prom_val_mer_{condicion}_{titularidad}'] = prom_valor_mercado
+
     return df_part
 
 def prueba():
@@ -70,9 +70,9 @@ def prueba():
 
     # Definicion de variables
     pais = "argentina"
-    pais = "argentina_south_america"
+    # pais = "argentina_south_america"
     fill_data_with_flashcore = False
-    n_dias_player_data = 365
+    n_dias_player_data = 30
 
     # Levanto datasets de prueba
     df_part = pd.read_excel(f'/Users/nachomondino/Documents/GitHub/predictor-apuestas/p3_data_preparation/data/{pais}/df_part_cleaned.xlsx')
@@ -89,19 +89,24 @@ def prueba():
 
     # Calculo edad y minutos jugados por jugador en cada partido
     df_jug_part = construct_data.add_fecha(df_part, df_jug_part)
+    df_jug_part = construct_data.add_team(df_part, df_jug_part)
     df_jug_part = construct_data.determine_edad(df_jug_part)
     df_jug_part = construct_data.determine_min_played(df_jug_part)
     df_jug_part = construct_data.determine_player_var_en_ult_partidos(df_jug_part, 'min_played', n_dias=n_dias_player_data, tipo='sum')
     df_jug_part = construct_data.determine_player_var_en_ult_partidos(df_jug_part, 'rating', n_dias=n_dias_player_data, tipo='mean_pond', var_pond='min_played')
-    df_jug_part.to_excel('/Users/nachomondino/Desktop/df_jug_part_antes_de_int_sofifa.xlsx', index=False)
+    # df_jug_part.to_excel('/Users/nachomondino/Desktop/df_jug_part_antes_de_int_sofifa.xlsx', index=False)
 
-    # Integro sofifa
-    df_jug_sofifa = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/p2_data_understanding/data/England/entidad_jugadores.xlsx')
+    # Integro sofifa a df_jug_part
+    # df_jug_part = pd.read_excel('/Users/nachomondino/Desktop/df_jug_part_antes_de_int_sofifa.xlsx')
+    df_jug_sofifa = pd.read_excel('/Users/nachomondino/Documents/GitHub/predictor-apuestas/p2_data_understanding/data/argentina/entidad_jugadores.xlsx')
     df_jug_part = player_data_in_match(df_jug_part, df_jug_sofifa)
-    df_jug_part.to_excel('/Users/nachomondino/Desktop/df_jug_part_antes_int.xlsx', index=False)
+    # df_jug_part.to_excel('/Users/nachomondino/Desktop/df_jug_part_antes_int.xlsx', index=False)
 
     # Integro df_jug_part_integ (df_jug_part + df_jug) a df_part
+    # df_jug_part = pd.read_excel('/Users/nachomondino/Desktop/df_jug_part_antes_int.xlsx')
     df = map_player_to_part(df_jug_part, df_part)
+    df.to_excel('/Users/nachomondino/Desktop/df_integrated.xlsx', index=False)
+
 
 # Código que se ejecuta solo cuando el archivo se ejecuta directamente
 if __name__ == "__main__":
