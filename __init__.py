8f40587455800656bad7c7f4a6084a3e6ade07cb
Ignacio Mondino <65468142+nachomondino1@users.noreply.github.com>
2024-02-13 19:16:08 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,2 +0,0 @@
-# Ensure that there is an __init__.py file in the directory where your main.py is located. 
-# This file can be empty, but it's necessary for Python to recognize the directory as a package.
