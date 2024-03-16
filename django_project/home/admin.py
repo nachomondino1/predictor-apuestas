30b56a03515958f90046783694a8ab330d16745a
camondino <carolina@offerfit.ai>
2024-03-16 16:31:52 -0300

    finished with part 1 of toturial

--- django_project/home/admin.py
+++ django_project/home/admin.py
@@ -1,24 +1,25 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from . import models
 
 # Register your models here.
 
-"""
-    @admin.register(models.Predictions) # decorator or 
-class PredictionsAdmin(admin.ModelAdmin): # how we want to view or edit our products
+@admin.register(models.Prediction)
+class PredictionAdmin(admin.ModelAdmin): # how we want to view or edit our products
+    # autocomplete_fields = ['id_team_home','id_team_away']
     list_display = ['date', 'id_team_home', 'id_team_away', 'predicted_result', 'prediction_winner']
     # list_editable = ['predicted_result']
     # list_per_page = 10
-
+    list_filter = ['date','predicted_result']
+    ordering = ['id_team_home', 'id_team_away']
+    search_fields = ['id_team_home__istartswith', 'id_team_away__istartswith']
+    
     # @admin.display(ordering='prediction')
     def prediction_winner(self, prediction):
-        if prediction.prob_home_bm > prediction.prob_away_bm:
+        if prediction.predicted_result == '0' or  prediction.predicted_result == 'X':
+            return 'Draw'
+        elif prediction.predicted_result == '1':
             return 'Home'
-        return 'Away'
-
-"""
-@admin.register(models.Prediction)
-class PredictionAdmin(admin.ModelAdmin): # how we want to view or edit our products
-    list_display = ['date', 'id_team_home', 'id_team_away', 'predicted_result']
-    # list_editable = ['predicted_result']
-    # list_per_page = 10
+        else:
+            return 'Away'
