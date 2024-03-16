d6fa99af5e71df478b27070bff36fcbbefe57639
camondino <carolina@offerfit.ai>
2024-03-16 17:38:59 -0300

    fixed format

--- django_project/home/views.py
+++ django_project/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models import (
