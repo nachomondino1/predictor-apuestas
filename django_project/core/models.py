26c30590ec338b9ce10180d62b898ac394b36a70
camondino <carolina@offerfit.ai>
2024-04-27 10:05:25 -0300

    last changes

--- django_project/core/models.py
+++ django_project/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# At the begining of the project! 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
