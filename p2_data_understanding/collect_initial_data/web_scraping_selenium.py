9686c8f4621b6bc54fc4a6bcc7dc88799200f106
Ignacio Mondino <nachomondino1@gmail.com>
2024-06-25 15:18:45 -0300

    Sparse Checkout

--- p2_data_understanding/collect_initial_data/web_scraping_selenium.py
+++ p2_data_understanding/collect_initial_data/web_scraping_selenium.py
@@ -58,19 +58,14 @@ class Crawler:
         if headless:
             options.add_argument("--headless")
 
-        # Inicializo chromedriver
-        service = ChromeDriverManager().install()  # ChromeDriverManager(driver_version=chrome_version).install())
-        driver = webdriver.Chrome(service=Service(service), options=options)
-        print("Creacion de ChromeDriver con install()")
-       
-        # try:
-        #     service = ChromeDriverManager().install()  # ChromeDriverManager(driver_version=chrome_version).install())
-        #     driver = webdriver.Chrome(service=Service(service), options=options)
-        #     print("Creacion de ChromeDriver con install()")
-        # except:
-        #     service = '/Users/nachomondino/Documents/chromedriver' if path is None else path  # Ultima actualizacion: 3 Mayo 2024
-        #     driver = webdriver.Chrome(service=Service(service), options=options)
-        #     print("Falló la creacion del ChromeDriver usando .install(), por lo que, recurro a crearlo desde archivo ejectuable")
+        try:
+            service = ChromeDriverManager().install()  # ChromeDriverManager(driver_version=chrome_version).install())
+            driver = webdriver.Chrome(service=Service(service), options=options)
+            print("Creacion de ChromeDriver con install()")
+        except:
+            service = '/Users/nachomondino/Documents/chromedriver' if path is None else path  # Ultima actualizacion: 3 Mayo 2024
+            driver = webdriver.Chrome(service=Service(service), options=options)
+            print("Falló la creacion del ChromeDriver usando .install(), por lo que, recurro a crearlo desde archivo ejectuable")
 
         return driver
 
