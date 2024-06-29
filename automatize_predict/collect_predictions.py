d267198abbc503de895e805ffe08480d8ce83aea
Ignacio Mondino <nachomondino1@gmail.com>
2024-06-29 18:12:46 -0300

    Mejoro nombres y simplifico funciones en collect_predictions.py

--- automatize_predict/collect_predictions.py
+++ automatize_predict/collect_predictions.py
@@ -5,52 +5,42 @@ import json
 import pandas as pd
 from p6_deployment import main_next_matches
 
-def collect_predictions(d_run: dict, l_countries:list, n_days:float, df_hist):
+def collect_predictions(d_run: dict, l_countries:list, n_days:float, df_historial_predicciones:pd.DataFrame):
     """
     Recoleccion de predicciones de todos los paises
     """
     # Defino condiciones del analisis
-    df = pd.DataFrame()
+    df_predicciones = pd.DataFrame()
 
     # Por country
     for id_country in l_countries:
 
         # Extraigo, preparo y predigo proximos partidos
-        df_country = main_next_matches.main(d_run, id_country, n_days, export=d_run['export'])
+        df_predicciones_country = main_next_matches.main(d_run, id_country, n_days, export=d_run['export'])
 
         # Concatenar df_countries....
-        df = pd.concat([df, df_country], axis=0)
+        df_predicciones = pd.concat([df_predicciones, df_predicciones_country], axis=0)
 
-    # Guardo predicciones en historial
-    df_hist = save_historial_predictions(df, df_hist)
+    # Guardo predicciones en historial_predicciones
+    print(f"Antes de cargar historial: {df_historial_predicciones.shape}")
+    df_historial_predicciones = pd.concat([df_historial_predicciones, df_predicciones], axis=0)
+    df_historial_predicciones = df_historial_predicciones[~df_historial_predicciones.index.duplicated(keep='last')]  # Eliminar filas con índices duplicados, manteniendo la ultima aparición (la + actualizada)
+    print(f"Despues de cargar historial: {df_historial_predicciones.shape}")
 
     # Exporto datos
-    df.index.name = 'id_match'  # Es importante para la base de datos MySQL
-    df.to_excel(f'p6_deployment/data/predicciones.xlsx', index=True)
-    df_hist.to_excel(f'p6_deployment/data/historial_predicciones.xlsx', index=True)
-
-def save_historial_predictions(df, df_hist):  # Lo hace bien!
-    """
-    Guarda predicciones en historial de predicciones
-    """
-    print(f"Antes de cargar historial: {df_hist.shape}")
-    # Concateno predicciones a historial
-    df_hist = pd.concat([df_hist, df], axis=0)
-
-    # Eliminar filas con índices duplicados, manteniendo solo la primera aparición
-    df_hist = df_hist[~df_hist.index.duplicated(keep='last')]
-    print(f"Despues de cargar historial: {df_hist.shape}")
-    return df_hist
+    df_predicciones.index.name = 'id_match'  # Es importante para la base de datos MySQL
+    df_predicciones.to_excel(f'p6_deployment/data/predicciones.xlsx', index=True)
+    df_historial_predicciones.to_excel(f'p6_deployment/data/historial_predicciones.xlsx', index=True)
 
 # Código que se ejecuta solo cuando el archivo se ejecuta directamente
 if __name__ == "__main__":
 
     # Defino argumentos
-    n_days = float(sys.argv[1])  # e.g. 7  # Numero de dias maximo desde hoy para extraer partidos
-    l_countries = ast.literal_eval(sys.argv[2])  # e.g. [48, 55, 59, 77, 167]
-    d_run = json.loads(sys.argv[3])  # Convertir la cadena JSON de vuelta a un diccionario  {'run_missing': True, 'data_unders': False, 'data_prep': False, 'modeling': False, 'export': True}
+    n_days = float(sys.argv[1])  # Numero de dias maximo desde hoy para extraer partidos (e.g. 7)
+    l_countries = ast.literal_eval(sys.argv[2])  # Lista de paises a los cuales extraer proximos partidos (e.g. [48, 55, 59, 77, 167])
+    d_run = json.loads(sys.argv[3])  # Parametros de ejecucion (e.g. {'run_missing': True, 'data_unders': False, 'data_prep': False, 'modeling': False, 'export': True})
 
     # Levanto historial_predicciones.xlsx
-    df_hist = pd.read_excel(f'p6_deployment/data/historial_predicciones.xlsx', index_col=0)
+    df_historial_predicciones = pd.read_excel(f'p6_deployment/data/historial_predicciones.xlsx', index_col=0)
 
-    collect_predictions(d_run, l_countries, n_days, df_hist)    
+    collect_predictions(d_run, l_countries, n_days, df_historial_predicciones)    
