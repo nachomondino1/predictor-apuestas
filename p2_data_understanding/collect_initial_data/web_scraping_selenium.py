2beccef72d2a411b8921aeb540a40244e1ccbf9a
Ignacio Mondino <nachomondino1@gmail.com>
2024-08-06 13:02:42 -0300

    Uso requierments.txt en vez de requirements_mnm.txt para ver si es problema de una libreria.

--- p2_data_understanding/collect_initial_data/web_scraping_selenium.py
+++ p2_data_understanding/collect_initial_data/web_scraping_selenium.py
@@ -89,15 +89,15 @@ class Crawler:
         options.add_argument("--disable-popup-blocking")
         options.add_argument("--remote-debugging-port=9222")
 
-        # if headless:
-        options.add_argument("--headless")
+        if headless:
+            options.add_argument("--headless")
 
         try:
-            os_manager = OperationSystemManager(os_type="linux64")
-            chrome_driver = ChromeDriverManager(os_system_manager=os_manager).install()
+            # os_manager = OperationSystemManager(os_type="linux64")
+            # chrome_driver = ChromeDriverManager(os_system_manager=os_manager).install()
 
             # Intentar instalar la última versión del ChromeDriver
-            # chrome_driver = ChromeDriverManager().install()
+            chrome_driver = ChromeDriverManager().install()
             driver = webdriver.Chrome(service=Service(chrome_driver), options=options)
             logger.info("ChromeDriver initialized with the latest version")
             return driver
