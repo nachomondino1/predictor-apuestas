9b3dde8f36342b0ab2d1870ee8003d80e90c6308
Ignacio Mondino <nachomondino1@gmail.com>
2024-08-25 17:16:24 -0300

    Actualizo predicciones.xlsx

--- p4_modeling/asses_model.py
+++ p4_modeling/asses_model.py
@@ -324,8 +324,11 @@ def calculate_multiplier(df: pd.DataFrame,  type_relation: str = 'equal', p1: tu
             m = (y2-y1) / (x2-x1)
             b = y1 - m*x1
 
+        # Limita los valores de 'dif_prob_result_to_bet' a un rango de -0.15 a 0.15
+        df['dif_prob_limited'] = np.clip(df['dif_prob_result_to_bet'], -0.15, 0.15)
+
         # Forma nuevo 2 y nuevo 3
-        df['multiplier'] = (df['prob_result_to_bet'] + df['dif_prob_result_to_bet']) * m + b
+        df['multiplier'] = (df['prob_result_to_bet'] + df['dif_prob_limited']) * m + b
 
         # Ajusta el multiplier para que sea menor a 100
         df['multiplier'] = np.where(
@@ -348,8 +351,11 @@ def calculate_multiplier(df: pd.DataFrame,  type_relation: str = 'equal', p1: tu
         # Calcular b a partir de su logaritmo
         b = np.exp(log_b)     
         
+        # Limita los valores de 'dif_prob_result_to_bet' a un rango de -0.15 a 0.15
+        df['dif_prob_limited'] = np.clip(df['dif_prob_mod_bm'], -0.35, 0.10)  # dif_prob_result_to_bet
+
         # Forma nuevo 2 y nuevo 3
-        df['multiplier'] = a * (b ** (df['prob_result_to_bet'] + df['dif_prob_result_to_bet']))
+        df['multiplier'] = a * (b ** (df['prob_result_to_bet'] + df['dif_prob_limited']))
 
         # Ajusta el multiplier para que sea menor a 100
         df['multiplier'] = np.where(
