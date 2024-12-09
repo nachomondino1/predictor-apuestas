f82bdf5da35c40877ad5e8aefa9d869d6856c1c2
Ignacio Mondino <nachomondino1@gmail.com>
2024-12-09 14:43:05 -0300

    Actualizo argumentos en mnm.py

--- p6_deployment/automatize_predict/collect_predictions.py
+++ p6_deployment/automatize_predict/collect_predictions.py
@@ -20,7 +20,7 @@ def collect_predictions(d_run: dict, l_countries:list, n_days:float, df_historia
     for id_country in l_countries:
 
         # Extraigo, preparo y predigo proximos partidos
-        df_predicciones_country = main_next_matches.main(d_run, id_country, n_days, export=d_run['export'])
+        df_predicciones_country = main_next_matches.main(d_run, id_country, n_days_max_next_matches=n_days, d_model=None, export=d_run['export'])
 
         # Elimino predicciones sin id_country y columnas vacias (las variables predictoras como referee)
         if len(df_predicciones_country) > 0:
@@ -59,10 +59,11 @@ if __name__ == "__main__":
     # Defino argumentos (no puedo usar variables entorno por update_predictions.yml que usa parametros especificos para cada corrida)
     if env == 'dev':
         # Definir condiciones del análisis
-        n_days = 13
-        l_countries = [48, 55, 59, 77, 148]
-        d_run = {'run_missing': True, 'data_unders': True, 'data_prep': True, 'modeling': True, 'export': True}
-        # d_run = {'run_missing': False, 'data_unders': False, 'data_prep': True, 'modeling': True, 'export': True}
+        n_days = 15
+        l_countries = [48, 55, 59, 77, 148] # 55
+        # l_countries = [148]
+        # d_run = {'run_missing': True, 'data_unders': False, 'data_prep': False, 'modeling': False, 'export': True}  # Solo missing
+        d_run = {'run_missing': False, 'data_unders': True, 'data_prep': True, 'modeling': True, 'export': True}   # Prod
 
     elif env == 'prod':
         n_days = float(sys.argv[1])  # Numero de dias maximo desde hoy para extraer partidos (e.g. 7)
