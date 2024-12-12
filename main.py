6248cff01b818cafc5e7c4f4ccbef7e936e6ef8a
Ignacio Mondino <nachomondino1@gmail.com>
2024-12-12 14:24:26 -0300

    DATA. Actualizo predicciones con nuevos modelos ya independizando estrategia de apuesta del entrenamiento

--- main.py
+++ main.py
@@ -408,9 +408,9 @@ class DataPreparation:
 
             # VARIABLES DERIVADAS
             # Expected Result and Expected Points (xPts) (from Expected Goals)
-            # goals_to_xg_ratio_ajustado = construct_data.adjust_thr_to_match_distributions(df, initial_thr=0.25, tolerance=0.05)
+            # goals_to_xg_ratio_ajustado = construct_data.adjust_ratio_to_match_distributions(df, initial_ratio=0.27, tolerance=0.08)
             # df = construct_data.determine_expected_result(df, goals_to_xg_ratio=goals_to_xg_ratio_ajustado)
-            df = construct_data.determine_expected_result(df, goals_to_xg_ratio=0.3, verbose=1) # 0.32 en GER y tolerance 7%
+            df = construct_data.determine_expected_result(df, goals_to_xg_ratio=0.3, verbose=1) # 0.32 en GER y tolerance 7%. FRA: 0.27 y tol 0.08
             df = construct_data.determine_expected_points(df)
             # df = df.drop(['expected_result'], axis=1) 
 
@@ -1123,7 +1123,7 @@ class Modeling:
 
         # Calculo ROI
         df_predicciones = construct_data.determine_expected_result(df_predicciones, goals_to_xg_ratio=0.3) # Intento hacerlo antes con df_match pero rompia.
-        df_predicciones, d_roi = bs.calculate_roi_by_betting_strategy(df_predicciones, strategy='train', save_strategy=False)
+        d_hiper, df_predicciones, d_roi = bs.calculate_roi_by_betting_strategy(df_predicciones, strategy='train')
         d_metrics.update(d_roi)
 
        # Convierto ids de equipos a nombres --> Hacerlo afuera de def assess_model...
