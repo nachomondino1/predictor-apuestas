a6041b6e69f17a39e437b3815ae5879d5f049cb4
Ignacio Mondino <nachomondino1@gmail.com>
2025-01-09 11:46:51 -0300

    Verifico diferencia entre assess y prod en assess_models_in_prod.

--- p4_modeling/assess_models_in_prod.py
+++ p4_modeling/assess_models_in_prod.py
@@ -3,16 +3,15 @@ sys.path.append('.')  # Fallaba el import de main
 from utils import directories
 from utils.set_up_logging import logger
 import pandas as pd
-import os
 from dotenv import load_dotenv
 from tqdm import tqdm
 from p3_data_preparation import format_data
 from p3_data_preparation.construct_data import determine_result, determine_expected_result
-from p4_modeling import asses_model, betting_strategy # ImportError: cannot import name 'DataPreparation' from partially initialized module 'main' (most likely due to a circular import) (/Users/nachomondino/Documents/GitHub/predictor-apuestas/main.py)
+from p4_modeling import asses_model, betting_strategy, compare_assess_prod
 from p6_deployment import main_next_matches
 
 
-def update_predicciones_test_with_missing(df_ite, id_country, country, iteration_date, path_save):
+def update_test_with_missing(df_ite, id_country, country, iteration_date, path_save):
     """
     Creo un df_iteration actualizado con las predicciones de test y las de missing. Actualiza el input para la seleccion de modelos o estrategia de apuesta.
 
@@ -28,76 +27,70 @@ def update_predicciones_test_with_missing(df_ite, id_country, country, iteration
     df_test = pd.DataFrame()    
     bs = betting_strategy.BettingStrategy() # no le paso iteration_date para que no guarde datos
     progress_bar = tqdm(total=len(df_ite), ncols=80)  # Inicializo barra de progreso
-
     base_path_dp = f'./data/{country}/p3_data_preparation/{iteration_date}'
+
+    # Defino que n_model use en prod (para comparar assess y prod)
+    n_model_prod, _, _ = main_next_matches.read_data_of_best_model(id_country)
 
     # Por modelo
     for idx, row in df_ite.iterrows():
 
-        if 'n_iteration' in df_ite.columns:
-            n_model =  row['n_iteration']
-        else:
-            n_model = idx
+        n_model = row['n_iteration'] if 'n_iteration' in df_ite.columns else idx
         model_name = row['model_name']
-        # if self.verbose >= 1:
-        #     logger.info(f'n_model: {n_model} model_name: {model_name}')
+        logger.info(f'n_model: {n_model} model_name: {model_name}')
 
         # Levanto df_predicciones de test
         df_pred = pd.read_excel(f"data/{country}/p4_modeling/{iteration_date}/models/{n_model}__{model_name}_predicciones.xlsx", index_col=0)
         logger.info(df_pred.shape)
 
-        # Recolecta predicciones de modelo en partidos "missing"
-        df_pred_missing = collect_model_prediction_in_missing_matches(id_country, n_model, model_name, iteration_date)
+        # Predict missing
+        df_pred_missing = predict_missing(id_country, n_model, model_name, iteration_date)
 
-        if isinstance(df_pred_missing, pd.DataFrame):
-            # if len(df_pred_missing) > 0:
-            #  Agrego columnas result y expected result
-            df_pred_missing = prepare_for_betting_strategy(df_pred_missing, country)  
+        # DIF ASSESS Y PROD (ACA ESTA OK? FUNCIONA?)
+        # Si el modelo fue utilizado en produccion:
+        if n_model == n_model_prod:
+            compare_assess_prod.compare_preparation(n_model, export=True)
+            compare_assess_prod.compare_modeling(n_model, export=True)
 
-            # Concateno df_pred y df_pred missing.
-            df_predicciones = pd.concat([df_pred, df_pred_missing], axis=0)
+        #  Agrego columnas result y expected result
+        df_pred_missing = determine_results(df_pred_missing, country)  
 
-        else:
-            logger.warning("No hay predicciones de partidos missing...")
-            df_predicciones = df_pred.copy()
-
-        # Volver a calcular metricas con test + missing
+        # Concateno df_pred y df_pred missing.
+        df_predicciones = pd.concat([df_pred, df_pred_missing], axis=0)
         df_predicciones = df_predicciones.loc[:, ['result', 'predicted_result', 'prob_class_1', 'prob_class_0', 'prob_class_2']]  # Elimino metricas del df_test viejo (dejo el df_pred_proba raso...)
+
+        # Recalculo metricas con test + missing
         df_filled = pd.read_excel(f'{base_path_dp}/treat_nan/df_filled_columns.xlsx', index_col=0) #  para calcular relleno..
         df_predicciones, d_metrics = asses_model.calculate_metrics(df_predicciones, country=country, df_filled=df_filled, export=False)  # --> Sobreescribe metricas de df_pred...
-        # print("C", df_predicciones)
-        # df_predicciones.to_excel('/Users/nachomondino/Desktop/df_iteration_test_3.xlsx')
-
-        # Calculo ROI y metricas que faltan
-        df_predicciones = determine_expected_result(df_predicciones, goals_to_xg_ratio=0.42) # Intento hacerlo antes con df_match pero rompia.
+        # df_predicciones = determine_expected_result(df_predicciones, goals_to_xg_ratio=0.42) # Intento hacerlo antes con df_match pero rompia.
         df_predicciones, _, d_roi = bs.calculate_roi_in_combinations(df_predicciones, strategy='train') # Chequear que funciona...
         d_metrics.update(d_roi)
         d_metrics.update(asses_model.calculate_advanced_metrics(df_predicciones=df_predicciones))
 
-        # Convierto ids de equipos a nombres --> Hacerlo afuera de def assess_model...
+        # Convierto ids de equipos a nombres
         df_teams = pd.read_excel(f'{base_path_dp}/integrate_data/df_teams.xlsx', index_col=0)
         df_predicciones = format_data.map_teams(df_predicciones, df_teams=df_teams)
 
-        # Hiperparametros del modelo y Metricas en testeo y train
-        row_test = {'n_iteration': n_model, 'model_name': model_name}
-        row_test.update(d_metrics)
+        # Guardo datos + Exporto
+        row_test = {'n_iteration': n_model, 'model_name': model_name, **d_metrics}
         df_row_test = pd.DataFrame([row_test])  # 1. Convertir el diccionario d_metrics en un DataFrame de una fila
         df_test = pd.concat([df_test, df_row_test], ignore_index=True)  # 2. Concatenar este nuevo DataFrame con df_metrics existente
-
         # Exporto datos del modelo
         df_test.to_excel(f'{path_save}/df_iteration_test.xlsx', index=False)
         df_predicciones.to_excel(f'{path_save}/{n_model}__{model_name}_predicciones.xlsx', index=True)
+        
         progress_bar.update(1)
 
     progress_bar.close()
+
     # Exporto df_iteration actualizado
     df_ite = df_ite.rename(columns={col: f"{col}_train" for col in df_ite.columns if col != 'n_iteration'})
     df_ite_updated = pd.merge(df_ite, df_test, on='n_iteration', how='outer')  # Concateno df_test actualizado con df_ite
     df_ite_updated.to_excel(f'{path_save}/df_iteration.xlsx', index=False)
 
     return df_ite_updated
-
-def collect_model_prediction_in_missing_matches(id_country, n_model, model_name, iteration_date): # No se si funciona ok el run_missing
+        
+def predict_missing(id_country, n_model, model_name, iteration_date): # No se si funciona ok el run_missing
     """
     Recolecta predicciones de modelos en partidos "missing"
 
@@ -109,23 +102,25 @@ def collect_model_prediction_in_missing_matches(id_country, n_model, model_name,
 
     # Usar mnm.py con predict_missing=True y data_unders=False.
     df = main_next_matches.main(d_run, id_country, d_model=d_model, predict_missing=True, export=False, verbose=0) 
+
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("No se generó un dataframe.")
+
+    elif len(df) == 0:
+        logger.warning("No hay predicciones de partidos missing...")
+        raise ValueError
+
     return df
 
-def prepare_for_betting_strategy(df, country):  # Ponerlo como funcion dentro de BettingStrategy????
+def determine_results(df, country):  # Ponerlo como funcion dentro de BettingStrategy????
     """
     Agregar columnas result y acerte en df_pred. Determino result y expected result para poder determinar "acerte"
     """
     ## Levanto df_match_miss para obtener goals? ??
     df_match_miss = pd.read_excel(f"data/{country}/p6_deployment/missing/data_understanding/all/df_match_miss.xlsx", index_col=0)
-
-    # Loggear información básica del DataFrame
-    # logger.info(df_match_miss)
-    # logger.info(f"Shape of df_match_miss: {df_match_miss.shape}")
-    # logger.info(f"Columns in df_match_miss: {df_match_miss.columns.tolist()}")
-
-    # Columnas a copiar
-    l_columns_to_copy = ['goals_home', 'goals_away', 'expected_goals_(xg)_home', 'expected_goals_(xg)_away']
-
+    l_columns_to_copy = ['goals_home', 'goals_away', 'expected_goals_(xg)_home', 'expected_goals_(xg)_away']   # Columnas a copiar
+    
+    # Ordeno df por date
     df = df.sort_values(by='date', ascending=False)
 
     # Asignar valores de df_match_miss a df solo en las columnas y filas correspondientes
@@ -135,17 +130,9 @@ def prepare_for_betting_strategy(df, country):  # Ponerlo como funcion dentro de
 
             df.loc[idx, column] = df_match_miss.loc[idx, column] 
 
-    # df.loc[df_match_miss.index, l_columns_to_copy] = df_match_miss[l_columns_to_copy]
-
     ## Determino result y expected result segun goals
     df = determine_result(df) # Intento hacerlo antes con df_match pero rompia.
     df = determine_expected_result(df, goals_to_xg_ratio=0.42) # Intento hacerlo antes con df_match pero rompia.
-
-    # Por que no calculo ROI y expected roi --> No deberia pues ahora calculo metricas del df_concatenado.. --> Creo que no ahce falta gracias a calculate_roi_by_betting_strategy()
-   
-    # # Agrego predicciones de bookmaker --> no tengo df_match_odds
-    # df_match_odds = asses_model.calculate_result_probabilities_by_bookmaker(df_match_odds) # Caculo probabilidades segun casa de apuesta
-    # df_match_odds = asses_model.determine_result_by_bookmaker(df_match_odds, self.var_pred_bm)  # Determino resultado predicho segun cuota minima (e.g. "Home")
     return df
 
 # Código que se ejecuta solo cuando el archivo se ejecuta directamente
