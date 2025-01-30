c7000851a03af213006e949d2247902434e03de4
Ignacio Mondino <nachomondino1@gmail.com>
2025-01-30 12:08:36 -0300

    Cambios menores

--- p4_modeling/main_select_model.py
+++ p4_modeling/main_select_model.py
@@ -86,7 +86,7 @@ def main(
     bs = betting_strategy.BettingStrategy(country, iteration_date, d_paths=d_paths, verbose=0)
     d_params = bs.define_hiperparameters(strategy=strategy)
     ## Seleccion de modelo
-    l_metrics = ['ROI_sin_bank_sin_ea', 'roi_last_25_matches_sin_ea', 'roi_last_25_matches_con_ea', 'ROI_sin_bank_con_ea']  ## Determino componentes de metrica combinada y pesos 
+    l_metrics = ['ROI_sin_bank_sin_ea', 'ROI_sin_bank_con_ea', 'roi_last_25_matches_sin_ea', 'roi_last_25_matches_con_ea'] # 'ROI_sin_bank_con_ea'## Determino componentes de metrica combinada y pesos 
     l_weights = [1 / len(l_metrics) for _ in l_metrics]
 
     # (0) Actualizo missing
@@ -132,7 +132,7 @@ def main(
                 df_pred.to_excel(f'{d_paths['path_assess']}/{n_model}__{model_name}_predicciones.xlsx', index=True) # sin ea pero con metricas
             
             # Defino estrategia
-            df, df_pred_with_stra = bs.define_model_betting_strategy_by_result(df_pred, col_to_max='roi', d_params=d_params) # No usar G/P_sin bank, toma estrategia con m maximo en todos los rdos.. no se por que
+            df, df_pred_with_stra = bs.define_model_betting_strategy_by_result(df_pred, d_params=d_params) # No usar G/P_sin bank, toma estrategia con m maximo en todos los rdos.. no se por que
 
             # Calculo metricas para guardar en df_ite_bs
             ## Calculo ROIs en last matches 
@@ -140,16 +140,16 @@ def main(
             roi_values_con_ea = asses_model.calculate_last_matches_roi(df_pred_with_stra, l_last_matches=[25, 50], extension='con_ea') # Con ea
             ## Calculo ROI con y sin ea
             roi_sin_bank_sin_ea, roi_sin_bank_con_ea = df_pred['G/P_sin_bank'].sum(), df_pred_with_stra['G/P_sin_bank'].sum()
-            roi_con_ea = df_pred_with_stra['G/P'].sum()
-            multiplicador = (roi_sin_bank_con_ea - roi_sin_bank_sin_ea) / abs(roi_sin_bank_sin_ea)
+            roi_sin_ea, roi_con_ea = df_pred['G/P'].sum(), df_pred_with_stra['G/P'].sum()
+            multiplicador = (roi_con_ea - roi_sin_ea) / abs(roi_sin_ea)
             
             # Guardo datos
             new_row = {
                 'n_model': n_model, 'model_name': model_name, 
                 **d_metric,
                 **roi_values_sin_ea, **roi_values_con_ea, 
-                'ROI_sin_bank_sin_ea': roi_sin_bank_sin_ea, 'ROI_sin_bank_con_ea': roi_sin_bank_con_ea, 'x ea': multiplicador,
-                'ROI_con_ea': roi_con_ea, 
+                'ROI_sin_bank_sin_ea': roi_sin_bank_sin_ea, 'ROI_sin_bank_con_ea': roi_sin_bank_con_ea, 
+                'ROI_sin_ea': roi_sin_ea, 'ROI_con_ea': roi_con_ea, 'x ea': multiplicador,
                 }
             rows.append(new_row)
             d_rows[n_model] = [df, df_pred_with_stra]
@@ -187,7 +187,7 @@ def main(
 if __name__ == "__main__":
     # Defino parametros
     l_countries = [48, 55, 59, 77, 148]
-    # l_countries = [148]
+    # l_countries = [59]
     
     # Defino hiperparametros
     update_missing = False  # Extract missing + Prepare missing
