3e417a8ba80af5af2b10c92cce25786e7149cd07
Ignacio Mondino <nachomondino1@gmail.com>
2025-01-31 11:13:49 -0300

    Metrica para seleccionar modelo: Agrego roi en ultimos 50 partidos

--- p4_modeling/main_select_model.py
+++ p4_modeling/main_select_model.py
@@ -165,7 +165,7 @@ def main(
 
     # (3) SELECCION DEL MODELO (el que maximiza el ROI con ea)
     ## Seleccion de modelo
-    l_metrics = ['roi_last_25_matches_sin_ea', 'ROI_sin_ea'] # 'roi_last_50_matches_sin_ea', # No usar Estrategia de apuesta para seleccionar modelo para evitar ruido de esta en la seleccion (posible mala seleccion x overfitting en ea)
+    l_metrics = ['roi_last_25_matches_sin_ea', 'roi_last_50_matches_sin_ea', 'ROI_sin_ea']
     l_weights = [1 / len(l_metrics) for _ in l_metrics]
     ## Calculo metrica combinada
     metric_col = 'metric'
@@ -188,12 +188,12 @@ def main(
 if __name__ == "__main__":
     # Defino parametros
     l_countries = [48, 55, 59, 77, 148]
-    l_countries = [55, 59, 77, 148]
+    # l_countries = [6]
     
     # Defino hiperparametros
     update_missing = False  # Extract missing + Prepare missing
     betting_strat = True # Recalcular estrategia de apuesta por modelo 
-    predict_missing = True # Predecir missing x modelo. betting_strategy debe ser True.
+    predict_missing = False # Predecir missing x modelo. betting_strategy debe ser True.
     export = True
 
     d_countries = {
