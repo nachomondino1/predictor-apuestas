30ae8b99a7e0acbf582705a3fc19ee7f195e8aa5
Ignacio Mondino <65468142+nachomondino1@users.noreply.github.com>
2025-02-13 12:40:03 -0300

    Vuelvo a usar ROI_sin_ea para seleccionar modelo

--- p4_modeling/main_select_model.py
+++ p4_modeling/main_select_model.py
@@ -82,16 +82,20 @@ def main(
 
 
     ## (0) Determino metricas para seleccionar modelos candidatos (1) y modelos en prod (3)
-    l_metrics = ['recall', 'recall_last_50', 'recall_last_25'] 
-    l_weights = [0, 1, 2]
+    # l_metrics = ['recall', 'recall_last_50', 'recall_last_25']  # Segun el exp, es lo que conviene. Me gustaria que no cambie tanto el modelo
+    # l_metrics = ['roi_sin_ea_last_50', 'roi_sin_ea_last_25']  # Antes usaba ROI...
+    l_metrics = ['roi_sin_ea_last_50', 'recall_last_50', 'roi_sin_ea_last_25', 'recall_last_25']  # Combinacion. 
+    l_weights = [1, 1, 1, 1]
 
 
     # (1) SELECCION DE MODELOS CANDIDATOS
     sbm = select_model_for_prod.SelectBestModel(id_country=id_country, path_save=d_paths['path_select'])  # Creo instancia de sbm
 
     # 1.0. Temporalmente, filtro l_metrics hasta que  tenga 'recall_last_50' y 'recall_last_25' en df_test (por ende en df_ite) --> El proximo train ya deberia tenerlo
-    l_metrics_filt = [metric for metric in l_metrics if metric in df_ite.columns]   
-    l_weights_filt = [1 / len(l_metrics_filt) for _ in l_metrics_filt]
+    l_metrics_filt = ['roi', 'recall']   # Roi sin ea...
+    l_weights_filt = [1, 1]
+    # l_metrics_filt = [metric for metric in l_metrics if metric in df_ite.columns]   
+    # l_weights_filt = [1 / len(l_metrics_filt) for _ in l_metrics_filt]
     logger.warning(l_metrics_filt)
 
     # 1.1. Calculo metrica combinada
@@ -99,7 +103,7 @@ def main(
     df_ite = asses_model.calculate_combined_metric(df_ite, l_metrics=l_metrics_filt, l_weights=l_weights_filt, metric_name=metric)
 
     ## 1.2. Filtro modelos segun metrica (y no por ROI para evitar modelos con alto ROI pero predicciones malas)
-    df_ite_filt = sbm.filter_models_by_metric(df_ite, metric_col=metric, n_models_max=20)
+    df_ite_filt = sbm.filter_models_by_metric(df_ite, metric_col=metric, n_models_max=30)
     logger.warning(f'Shape: {df_ite.shape} --> {df_ite_filt.shape}')
 
 
@@ -204,7 +208,8 @@ def main(
 if __name__ == "__main__":
     # Defino parametros
     l_countries = [48, 55, 59, 77, 148]
-    l_countries = [6]
+    # l_countries = [55, 59, 77, 148]
+    # l_countries = [48]
 
     # Defino hiperparametros
     assess = True
@@ -214,14 +219,6 @@ if __name__ == "__main__":
     export = True
 
     d_countries = {
-        # Train actuales
-        # 6: ["argentina", '2025-01-28'], 
-        # 48: ["england", '2025-01-22'],
-        # 55: ["france", '2025-01-22'], 
-        # 59: ["germany", '2025-01-23'], 
-        # 77: ["italy", '2025-01-20'],
-        # 148: ["spain", '2025-01-20'], 
-        # 167: ["usa", '2024-12-05'],
         # Train nuevos
         6: ["argentina", '2025-02-06'], 
         48: ["england", '2025-02-05'],
@@ -236,6 +233,7 @@ if __name__ == "__main__":
         iteration_date = d_countries[id_country][1]
         
         df_ite = pd.read_excel(f"data/{country}/p4_modeling/{iteration_date}/df_iteration.xlsx")
+        print(df_ite)
 
         main(
             df_ite=df_ite,
@@ -246,3 +244,4 @@ if __name__ == "__main__":
             betting_strat=betting_strat,
             export=export
             )
+        
