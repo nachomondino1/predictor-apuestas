f9a8550fec61e27d5cc37531bf6005507f09a154
Ignacio Mondino <nachomondino1@gmail.com>
2025-03-14 11:21:03 -0300

    Agrego m de 60 y 140

--- p4_modeling/betting_strategy.py
+++ p4_modeling/betting_strategy.py
@@ -41,7 +41,7 @@ class BettingStrategy:
         Defino hiperparametros de estrategia de apuesta a probar segun si apuesto como la realidad o no.
         """
         list_dp = [0, 0.45, 0.6, 0.75] if vary_dp else [0]  # el 0.4 esta muy cerca del cambio de result to bet entre assess y prod.
-        list_m = [10, 15, 20, 25, 30, 35, 40, 45, 50, 65, 80, 95, 110, 140, 170, 200] if big_space_m else [80, 100, 120] # [10, 20, 40, 60, 80, 100, 120] # no uso 200 por si acertó todo en ese rdo.
+        list_m = [10, 15, 20, 25, 30, 35, 40, 45, 50, 65, 80, 95, 110, 140, 170, 200] if big_space_m else [60, 80, 100, 120, 140] # [10, 20, 40, 60, 80, 100, 120] # no uso 200 por si acertó todo en ese rdo.
         
         if strategy == "train": # "Sin estrategia"
             dic = {
