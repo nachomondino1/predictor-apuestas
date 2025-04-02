961d81c5ef41ddc4eb64ba18a5738ce8759a4a91
Ignacio Mondino <nachomondino1@gmail.com>
2025-04-02 14:54:46 -0300

    Cambio metricas

--- p4_modeling/main_select_model.py
+++ p4_modeling/main_select_model.py
@@ -186,16 +186,21 @@ if __name__ == "__main__":
         }
     
     # Defino metricas y pesos
-    # 75-25
+#     d_metrics = {
+#        48: {'error': 0.25, 'f1_score_draw': 0.1, 'f1_score_away': 0.65},
+#        55: {'error': 0.25, 'f1_score_draw': 0.1, 'f1_score_away': 0.65},
+#        59: {'error': 0.25, 'f1_score_draw': 0.2,'f1_score_away': 0.55}, 
+#        77: {'error': 0.25, 'f1_score_draw': 0.35, 'f1_score_away': 0.4}, 
+#        148: {'error': 0.25, 'f1_score_draw': 0.75, 'f1_score_away': 0} 
+#    }
+  
     d_metrics = {
-       48: {'error': 0.25, 'f1_score_draw': 0.1, 'f1_score_away': 0.65},
-       55: {'error': 0.25, 'f1_score_draw': 0.1, 'f1_score_away': 0.65},
-       59: {'error': 0.25, 'f1_score_draw': 0.2,'f1_score_away': 0.55}, 
-       77: {'error': 0.25, 'f1_score_draw': 0.35, 'f1_score_away': 0.4}, 
-       148: {'error': 0.25, 'f1_score_draw': 0.75, 'f1_score_away': 0} 
+       48: {'n_away': 0.5, 'accuracy_away': 0.5},
+       55: {'n_away': 0.5, 'accuracy_away': 0.5},
+       59: {'n_away': 0.5, 'accuracy_away': 0.5},
+       77: {'n_draw': 0.5, 'accuracy_draw': 0.5},
+       148: {'n_draw': 0.5, 'accuracy_draw': 0.5}
    }
-    # l_metrics = ['error', 'roi'] 
-    # l_weights = [0.5, 0.5]
 
     for id_country in l_countries:
         country = d_countries[id_country][0]
