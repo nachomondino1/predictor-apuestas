e039313b7fb3b9b2beec01115d42a5275516955a
Ignacio Mondino <nachomondino1@gmail.com>
2025-04-09 10:34:26 -0300

    Cambios menores

--- p6_deployment/main_next_matches.py
+++ p6_deployment/main_next_matches.py
@@ -674,7 +674,7 @@ class TrainingDataLoader():
 
             user_input = str(input("Quiere predecir sin estrategia igual (y para aceptar)?: "))
             if user_input == 'y':
-                return {'prob_dp': -10000, 'curva': 'linear', 'm': 10, 'b': 0, 'k': 1}
+                return {'prob_dp': 0, 'curva': 'linear', 'm': 10, 'b': 0, 'k': 1}
             
             raise ValueError(e)
 
@@ -1213,7 +1213,7 @@ def main(
         # Predigo con modelo cargado
         if predict_missing:
             df_filled = df_fill.copy() 
-            d_strategy = {'prob_dp': -10000, 'curva': 'linear', 'm': 10, 'b': 0}
+            d_strategy = {'prob_dp': 0, 'curva': 'linear', 'm': 10, 'b': 0}
         else:  
             df_filled = pd.concat([df_c1['copiado_formaciones'], df_fill.loc[:, ['player_emergency_fill', 'emergency_fill']]], axis=1) 
             d_strategy = lo.load_modeling_hyperparameters()
@@ -1271,16 +1271,14 @@ if __name__ == "__main__":
 
     # Defino country, iteration date y modelo
     d_countries = {
-        # 6: ["argentina", '2025-02-06'], 
-        48: ["england", '2025-03-23'], 
-        55: ["france", '2025-03-23'], 
-        59: ["germany", '2025-03-23'], 
-        77: ["italy", '2025-03-23'],
-        148: ["spain", '2025-03-24'], 
-        # 167: ["usa", '2024-12-05']
+        48: ["england", '2025-04-08'], 
+        55: ["france", '2025-04-08'], 
+        59: ["germany", '2025-04-08'], 
+        77: ["italy", '2025-04-08'],
+        148: ["spain", '2025-04-08'], 
         }
     iteration_date = d_countries[id_country][1]
-    d_model = {'n_model': 1097, 'model_name': "LogisticRegression"} # DecisionTreeClassifier, XGBClassifier, neural_networ, SVC, LogisticRegression, MLPClassifier
+    d_model = {'n_model': 199, 'model_name': "SVC"} # SVC, LogisticRegression
 
     if key == 'missing':
         
