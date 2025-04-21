98a422326efb175cb7145f7fefaab153f89bb4cb
Ignacio Mondino <nachomondino1@gmail.com>
2025-04-21 12:04:30 -0300

    Actualizo recalculo de metricas

--- p4_modeling/utils_select_model/evaluate_test_with_new_metrics.py
+++ p4_modeling/utils_select_model/evaluate_test_with_new_metrics.py
@@ -64,31 +64,21 @@ def main(
 
     return df_ite_bs
 
-def concat_dfs(df_ite_train, df_ite_test, df_ite_new_test):
+def concat_dfs(df_ite, df_ite_train, df_ite_new_test):
+
+    # Merge de los DataFrames
+    df_iteration = pd.merge(df_ite, df_ite_train, on='n_iteration', how='outer')
 
     # Renombrar 'n_model' a 'n_iteration' en df_ite_new_test
     df_ite_new_test = df_ite_new_test.rename(columns={'n_model': 'n_iteration'})
 
-    # Identificar y eliminar columnas duplicadas en df_ite_test
-    cols_dif = [col for col in df_ite_new_test.columns if col in df_ite_test.columns]
-    cols_dif.remove("n_iteration")
-    cols_dif.remove("model_name")
-    df_ite_test.drop(columns=cols_dif, inplace=True)
-    print(df_ite_test.shape)
-
-    # Merge de los DataFrames
-    df_ite_test_comp = pd.merge(df_ite_test, df_ite_new_test, on='n_iteration', how='outer')
-    df_ite_test_comp.drop(columns=['model_name'], inplace=True) # df_train ya tiene model name
-
-    # 2. Concat train y test
-    df_iteration = pd.merge(df_ite_train, df_ite_test_comp, on='n_iteration', how='outer') 
-    print(df_iteration.shape)
+    df_iteration = pd.merge(df_iteration, df_ite_new_test, on=['n_iteration', 'model_name'], how='outer')
     return df_iteration
 
 if __name__ == "__main__":
     # Defino parametros
     l_countries = [48, 55, 59, 77, 148]
-    l_countries = [48]
+    l_countries = [55]
 
     d_countries = {
         # Train nuevos
@@ -104,18 +94,21 @@ if __name__ == "__main__":
         iteration_date = d_countries[id_country][1]
 
         # Levanto datos
-        df_ite = pd.read_excel(f"data/{country}/p4_modeling/{iteration_date}/df_iteration.xlsx")
+        df_ite = pd.read_excel(f"data/{country}/p4_modeling/{iteration_date}/df_ite.xlsx")
         df_ite_train = pd.read_excel(f"data/{country}/p4_modeling/{iteration_date}/df_ite_train.xlsx")
         df_ite_test = pd.read_excel(f"data/{country}/p4_modeling/{iteration_date}/df_ite_test.xlsx")
         print(df_ite.shape, df_ite_train.shape, df_ite_test.shape)
 
         # Calculo nuevas metricas en test
-        df_ite_test_new = main(df_ite=df_ite_test, country=country, iteration_date=iteration_date)
-        # df_ite_test_new = pd.read_excel(f'data/{country}/p4_modeling/{iteration_date}/df_iteration_test_new.xlsx')
-        print(df_ite_test_new.head(5))
+        reevaluate = True
+        if reevaluate:
+            df_ite_test_new = main(df_ite=df_ite_test, country=country, iteration_date=iteration_date)
+        else:
+            df_ite_test_new = pd.read_excel(f'data/{country}/p4_modeling/{iteration_date}/df_iteration_test_new.xlsx')
+            print(df_ite_test_new.head(5))
         
         # Concateno nuevo test a trian para generar el nuevo df_ite
-        df = concat_dfs(df_ite_train, df_ite_test, df_ite_test_new)
+        df = concat_dfs(df_ite, df_ite_train, df_ite_test_new)
         df.to_excel(f'data/{country}/p4_modeling/{iteration_date}/df_iteration_new.xlsx', index=False)
 
         
