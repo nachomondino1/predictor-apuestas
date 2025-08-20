79683a390a000518e6e7766e5e4e53e441fc6a8a
Ignacio Mondino <nachomondino1@gmail.com>
2025-08-20 00:16:16 -0300

    La nueva linea hace fallar en train

--- p4_modeling/asses_model.py
+++ p4_modeling/asses_model.py
@@ -32,8 +32,7 @@ def calculate_metrics(
     # Calculo métricas básicas
     d_metrics = {
         'error': -log_loss(y_test, y_pred_prob, labels=[0, 1, 2]),
-        'test_accuracy': accuracy_score(y_test, y_pred) * 100,
-        'test_accuracy_dp': df['acerte'].mean() * 100,
+        'test_accuracy': accuracy_score(y_test, y_pred) * 100, # 'test_accuracy_dp': df['acerte'].mean() * 100,
         'recall': recall_score(y_test, y_pred, average='macro') * 100,
         'f1_score': f1_score(y_test, y_pred, average='macro') * 100,
     }
