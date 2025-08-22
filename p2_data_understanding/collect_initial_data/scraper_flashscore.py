64bbce98cd277cb54fe72c3845e38ff545d62c4a
Ignacio Mondino <nachomondino1@gmail.com>
2025-08-22 20:39:32 -0300

    Fuerzo update

--- p2_data_understanding/collect_initial_data/scraper_flashscore.py
+++ p2_data_understanding/collect_initial_data/scraper_flashscore.py
@@ -969,7 +969,7 @@ def extract_next_matches(id_country, country: str, id_competicion, competition: 
 def extract_matches_result(country: str, competition: str, l_ids:list):
 
     # DEFINCION DE PARAMETROS & VARIABLES
-    crawler = FlashscoreCrawler(headless=True)
+    crawler = FlashscoreCrawler()
     df = pd.DataFrame()
 
     # Formateo variables para construir url
